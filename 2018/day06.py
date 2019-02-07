8affc003578aa495062c733003f7e8843d7406c1
Bernard Roesler <bernard.roesler@gmail.com>
2019-02-07 07:31:34 -0500

    Separate geometry.py into module.

--- 2018/day06.py
+++ 2018/day06.py
@@ -13,125 +13,12 @@ import re
 import numpy as np
 import matplotlib.pyplot as plt
 
+from geometry import Point
+
 pat = re.compile(r'(\d+), (\d+)')
 
-class Point():
-    # TODO write doctests
-    """Geometric point class."""
-    def __init__(self, c=[], kind='cartesian'):
-        """Create a Point object with given coordinates.
-
-        :param array-like c: shape (n,), Cartesian coordinate vector
-        :param str kind: 'cartesian', 'polar', etc. defines how input vector is
-            used to define shorthand attributes.
-        :returns: Point object
-        :rtype: Point
-        """
-        if kind.lower() == 'cartesian':
-            self.c = np.array(c)
-        elif kind.lower() == 'polar':
-            if len(c) != 2:
-                raise Exception('Polar only applicable to 2-D Points')
-            self.c = self.pol2cart(c[0], c[1])
-        # elif kind.lower() == 'spherical', etc.
-        else:
-            raise Exception(f'Invalid kind {kind}')
-
-    @property
-    def dims(self):
-        return self.c.size
-
-    @staticmethod
-    def pol2cart(r, theta):
-        """Convert polar coordinates to Cartesian coordinates."""
-        return np.array([r * np.cos(theta),
-                         r * np.sin(theta)])
-
-    #-------------------------------------------------------------------------- 
-    #        Shorthand for 2D or 3D points
-    #--------------------------------------------------------------------------
-    @property
-    def x(self):
-        return self.c[0] if self.dims > 0 else None
-
-    @x.setter
-    def x(self, val):
-        self.c[0] = val 
-
-    @property
-    def y(self):
-        return self.c[1] if self.dims > 1 else None
-
-    @y.setter
-    def y(self, val):
-        self.c[1] = val 
-
-    @property
-    def z(self):
-        return self.c[2] if self.dims > 2 else None
-
-    @z.setter
-    def z(self, val):
-        self.c[2] = val 
-
-    #-------------------------------------------------------------------------- 
-    #        Polar coors
-    #--------------------------------------------------------------------------
-    @property
-    def r(self):
-        """Euclidean distance from origin."""
-        return (np.sum(self.c**2))**0.5
-
-    @r.setter
-    def r(self, val):
-        """Assumes theta held constant."""
-        theta = getattr(self, 'theta', 0)
-        self.c = self.pol2cart(val, theta)
-
-    @property
-    def theta(self): 
-        """Polar angle from x-axis."""
-        if (self.dims != 2) and (self.dims != 3):
-            raise Exception('Property only applicable to 2-D and 3-D Points')
-        return np.arctan2(self.y, self.x)
-
-    @theta.setter
-    def theta(self, val):
-        """Assumes r held constant."""
-        if (self.dims != 2) and (self.dims != 3):
-            raise Exception('Property only applicable to 2-D and 3-D Points')
-        r = getattr(self, 'r', 1)
-        self.c = self.pol2cart(r, val)
-
-    @property
-    def theta_deg(self):
-        return (180 / np.pi) * self.theta
-
-    @staticmethod
-    def _pdist(c1, c2, p=2):
-        """p-norm distance for general p. Manhattan p = 1, Euclidean p = 2."""
-        return (np.sum(np.abs(c1 - c2)**p))**(1/p)
-
-    def _make_dist(self, p):
-        """Make a p-norm distance function of 2 arguments with parameter p."""
-        return lambda a, b: self._pdist(a, b, p=p)
-
-    def dist(self, p2, kind='euclidean'):
-        """Calculate distance between self and second Point."""
-        kinds = {'manhattan': self._make_dist(1),
-                 'euclidean': self._make_dist(2)}
-        func = kinds.get(kind.lower(), lambda x, y: f'Invalid kind {x}')
-        return func(self.c, p2.c)
-
-    def __str__(self):
-        return 'Point({}, {})'.format(self.x, self.y)
-
-    def __repr__(self):
-        return self.__str__()
-
-
 def parse(line):
-    """Convert string '123, 45\n' to tuple of integers (123, 45)."""
+    """Convert string '123, 45\n' to Point of integers Point(123, 45)."""
     data = line.rstrip()
     res = pat.match(data)
     x, y = int(res.group(1)), int(res.group(2))
@@ -145,18 +32,22 @@ with open(filename, 'r') as file:
 x, y, angles = np.array([(p.x, p.y, p.theta_deg) for p in coords]).T
 
 # Brute force:
-#  1. create integer grid that circumscribes max dimensions
-#  2. classify grid points via 1-NN
-#  3. count points labeled in each class
-#  4. take maximum count, excluding convex hull of centroids
+#  * compute Voronoi vertices
+#  * create integer grid that circumscribes max dimensions of Voronoi diagram
+#  * classify grid points via 1-NN
+#  * count points labeled in each class
+#  * take maximum count, excluding convex hull of centroids
 
-# xg, yg = 
+xg, yg = np.meshgrid(np.linspace(np.min(x), np.max(x), x.size),
+                     np.linspace(np.min(y), np.max(y), y.size),
+                     indexing='ij')
 
 #------------------------------------------------------------------------------ 
 #        Plots
 #------------------------------------------------------------------------------
 fig = plt.figure(1, clear=True)
 ax = fig.add_subplot(111)
+ax.scatter(xg, yg, c='k', s=1, alpha=0.2)
 sc = ax.scatter(x, y, c=angles, vmin=0, vmax=90)
 # ax.scatter(x[:2], y[:2], marker='x', c='r', s=100)  # highlight points
 
@@ -164,7 +55,7 @@ cb = plt.colorbar(sc)
 cb.ax.set_ylabel(r'$\theta$ [deg]')
 ax.set_xlabel(r'$x$')
 ax.set_ylabel(r'$y$')
-ax.grid()
+ax.grid(zorder=0)
 
 plt.show()
 #==============================================================================
---
+++ 2018/geometry.py
@@ -0,0 +1,179 @@
+#!/Users/bernardroesler/anaconda3/envs/insight/bin/python3
+#==============================================================================
+#     File: geometry.py
+#  Created: 2019-02-06 20:08
+#   Author: Bernie Roesler
+#
+"""
+  Description: Geometry objectcs and methods
+"""
+#==============================================================================
+
+import numpy as np
+
+#------------------------------------------------------------------------------ 
+#        Utilities
+#------------------------------------------------------------------------------
+def rad2deg(theta):
+    return (180 / np.pi) * theta
+
+def deg2rad(theta_deg):
+    return (np.pi / 180) * theta_deg
+
+def pol2cart(r, theta):
+    return np.array([r * np.cos(theta),
+                     r * np.sin(theta)])
+
+def cart2pol(x, y):
+    return np.array([(x**2 + y**2)**0.5,
+                     np.arctan2(y, x)])
+
+#------------------------------------------------------------------------------ 
+#        Classes
+#------------------------------------------------------------------------------
+class Point():
+    """Geometric point class.
+
+    Examples
+    --------
+    >>> origin = Point((0, 0))
+    >>> origin.x, origin.y, origin.z
+    (0.0, 0.0, None)
+    >>> p = Point((3, 4))
+    >>> p.x, p.y
+    (3.0, 4.0)
+    >>> p.r
+    5.0
+    >>> origin.dist(p)  # default Euclidean
+    5.0
+    >>> origin.dist(p, kind='manhattan')
+    7.0
+    >>> p2 = Point((1, 1))
+    >>> p2.r - np.sqrt(2)
+    0.0
+    >>> p2.theta - np.pi/4
+    0.0
+    >>> p2.theta_deg
+    45.0
+    """
+
+    def __init__(self, c=list(), kind='cartesian'):
+        """Create a Point object with given coordinates.
+
+        :param array-like c: shape (n,), coordinate vector
+        :param str kind: 'cartesian', 'polar', etc. defines how input vector is
+            used to define shorthand attributes.
+        :returns: Point object
+        :rtype: Point
+        """
+        if kind.lower() == 'cartesian':
+            self.c = np.array([float(x) for x in c])
+        elif kind.lower() == 'polar':
+            if len(c) != 2:
+                raise Exception('Polar only applicable to 2-D Points')
+            self.c = pol2cart(c[0], c[1])
+        # elif kind.lower() == 'spherical', etc.
+        else:
+            raise Exception(f'Invalid kind {kind}')
+
+    @property
+    def dims(self):
+        return self.c.size
+
+    #-------------------------------------------------------------------------- 
+    #        Shorthand for 2D or 3D points
+    #--------------------------------------------------------------------------
+    @property
+    def x(self):
+        return self.c[0] if self.dims > 0 else None
+
+    @x.setter
+    def x(self, val):
+        self.c[0] = val 
+
+    @property
+    def y(self):
+        return self.c[1] if self.dims > 1 else None
+
+    @y.setter
+    def y(self, val):
+        self.c[1] = val 
+
+    @property
+    def z(self):
+        return self.c[2] if self.dims > 2 else None
+
+    @z.setter
+    def z(self, val):
+        self.c[2] = val 
+
+    #-------------------------------------------------------------------------- 
+    #        Polar coords
+    #--------------------------------------------------------------------------
+    @property
+    def r(self):
+        """Euclidean distance from origin."""
+        return (np.sum(self.c**2))**0.5
+
+    @r.setter
+    def r(self, val):
+        """Assumes theta held constant."""
+        theta = getattr(self, 'theta', 0)
+        self.c = pol2cart(val, theta)
+
+    @property
+    def theta(self): 
+        """Polar angle from x-axis."""
+        if (self.dims != 2) and (self.dims != 3):
+            raise Exception('Property only applicable to 2-D and 3-D Points')
+        return np.arctan2(self.y, self.x)
+
+    @theta.setter
+    def theta(self, val):
+        """Assumes r held constant."""
+        if (self.dims != 2) and (self.dims != 3):
+            raise Exception('Property only applicable to 2-D and 3-D Points')
+        r = getattr(self, 'r', 1)
+        self.c = pol2cart(r, val)
+
+    @property
+    def theta_deg(self):
+        return rad2deg(self.theta)
+
+    #-------------------------------------------------------------------------- 
+    #        Distance
+    #--------------------------------------------------------------------------
+    @staticmethod
+    def _pdist(c1, c2, p=2):
+        """p-norm distance for general p. Manhattan p = 1, Euclidean p = 2."""
+        return (np.sum(np.abs(c1 - c2)**p))**(1/p)
+
+    def _make_dist(self, p):
+        """Make a p-norm distance function of 2 arguments with parameter p."""
+        return lambda a, b: self._pdist(a, b, p=p)
+
+    def dist(self, p2, kind='euclidean'):
+        """Calculate distance between self and second Point."""
+        if self.dims != p2.dims:
+            raise Exception("Points must have same dimensionality! ({} != {})"\
+                            .format(self.dims, p2.dims))
+        kinds = {'manhattan': self._make_dist(1),
+                 'euclidean': self._make_dist(2)}
+        func = kinds.get(kind.lower(), lambda x, y: f'Invalid kind {x}')
+        return func(self.c, p2.c)
+
+    #-------------------------------------------------------------------------- 
+    #        Utilities
+    #--------------------------------------------------------------------------
+    def __str__(self):
+        return 'Point({}, {})'.format(self.x, self.y)
+
+    def __repr__(self):
+        return self.__str__()
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
+#==============================================================================
+#==============================================================================
