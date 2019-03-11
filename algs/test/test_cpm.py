a89fd7e1efa80804639b93655e6200be8062dc0d
Bernard Roesler <bernard.roesler@gmail.com>
2019-03-10 23:59:07 -0400

    Update test_cpm.py output.

--- algs/test/test_cpm.py
+++ algs/test/test_cpm.py
@@ -18,28 +18,34 @@ with open(filename, 'r') as file:
     lines = iter(file.readlines())
     V = int(next(lines))
     for i, line in enumerate(lines):
-        # duration, n_prec, prec_a, prec_b, ...
+        # duration, n_prec, [must complete before]
         nums = iter(line.rstrip().split())
 
         duration = float(next(nums))
-        G.add_edge('source',   str(i), 0.0)    # source to the start of the job
-        G.add_edge(str(i)+'x', 'sink', 0.0)    # end of the job to the sink
-        G.add_edge(str(i), str(i)+'x', duration)  # duration of the job
+        G.add_edge('source',       str(i), 0.0)    # source to the start of the job
+        G.add_edge(str(i)+'x',     'sink', 0.0)    # end of the job to the sink
+        G.add_edge(str(i),     str(i)+'x', duration)  # duration of the job
 
-        # Add precedence constraints
+        # Add precedence constraints from end of task to start of next task
         for j in range(int(next(nums))):
             prec = str(next(nums))
             G.add_edge(str(i)+'x', prec, 0.0)
 
 # Find longest path from source to sink
 lp = AcyclicPath(G, 'source', kind='max')
+tasks = [str(i) for i in range(V)]
+#         job start          finish
+output = [(v, lp.dist_to(v), lp.dist_to(v+'x')) for v in tasks]
 
 print(" job   start  finish")
 print("--------------------")
-for idx in range(V):
-    i = str(idx)
-    print(f"{i:>4s} {lp.dist_to(i):7.1f} {lp.dist_to(i+'x'):7.1f}")
+# Sort by start time
+for v in sorted(output, key=lambda x: x[1]):
+    print("{:>4s} {:7.1f} {:7.1f}".format(*v))
 print(f"Finish time: {lp.dist_to('sink'):7.1f}")
+
+assert lp.dist_to('source') == 0.0
+assert lp.dist_to('sink') == 173.0
 
 #==============================================================================
 #==============================================================================
