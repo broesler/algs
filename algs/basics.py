6b848e37f81c539a3a77f4f7f10953c736694756
Bernard Roesler <bernard.roesler@gmail.com>
2019-11-04 20:00:56 -0500

    Convert tests to `should_be`s.

--- algs/basics.py
+++ algs/basics.py
@@ -589,26 +589,26 @@ if __name__ == '__main__':
     s = Stack()
     for i in range(5):
         s.push(i)
-    assert s.size == 5
-    assert not s.is_empty
-    assert 4 == s.peek()
-    assert 4 == s.pop()
+    should_be(s.size, 5)
+    should_be(s.is_empty, False)
+    should_be(4, s.peek())
+    should_be(4, s.pop())
     # Test iteration -- pop should be in reverse order
     for i, item in zip([3, 2, 1, 0], s):
-        assert i == item
+        should_be(i, item)
     # Test for pop
     err_test(s, 'pop')
 
     # Test Queue
     q = Queue(['A', 'B', 'C'])
     q.enqueue('D')
-    assert q.size == 4
-    assert not q.is_empty
-    assert 'A' == q.peek()
-    assert 'A' == q.dequeue()
+    should_be(q.size, 4)
+    should_be(q.is_empty, False)
+    should_be('A', q.peek())
+    should_be('A', q.dequeue())
     # Elements should be in forwards order
     for c, item in zip(['B', 'C', 'D'], q):
-        assert c == item
+        should_be(c, item)
     # Test dequeue error
     err_test(q, 'dequeue')
 
@@ -621,83 +621,88 @@ if __name__ == '__main__':
 
     # Test maxPQ
     pq = PriorityQueue(data, kind='max')
-    assert not pq.is_empty
-    assert pq.size == 26
-    assert 'Z' == pq.peek()
-    assert 'Z' == pq.dequeue()
-    assert 'Y' == pq.dequeue()
-    assert 'X' == pq.dequeue()
-    assert 'W' == pq.peek()
+    should_be(pq.is_empty, False)
+    should_be(pq.size, 26)
+    should_be('Z', pq.peek())
+    should_be('Z', pq.dequeue())
+    should_be('Y', pq.dequeue())
+    should_be('X', pq.dequeue())
+    should_be('W', pq.peek())
     for c in ['X', 'Y', 'Z']:
         pq.enqueue(c)
     # implicitly test iteration
-    assert ''.join(pq) == string.ascii_uppercase[::-1]
+    should_be(''.join(pq), string.ascii_uppercase[::-1])
     # Test dequeue error
     err_test(pq, 'dequeue')
 
     # Test MinPQ
     pq = PriorityQueue(data, kind='min')
-    assert 'A' == pq.dequeue()
-    assert 'B' == pq.dequeue()
-    assert 'C' == pq.dequeue()
-    assert 'D' == pq.peek()
+    should_be('A', pq.dequeue())
+    should_be('B', pq.dequeue())
+    should_be('C', pq.dequeue())
+    should_be('D', pq.peek())
     for c in ['A', 'B', 'C']:
         pq.enqueue(c)
     # implicitly test iteration
-    assert ''.join(pq) == string.ascii_uppercase
+    should_be(''.join(pq), string.ascii_uppercase)
 
     # Test IndexMinPQ
     pq = IndexPQ(zip(idx, data), kind='min')
-    assert len(pq) == 26
-    assert (0, 'A') == pq.dequeue()
-    assert (1, 'B') == pq.dequeue()
-    assert (2, 'C') == pq.dequeue()
-    assert len(pq) == 23
-    assert (3, 'D') == pq.peek()
+    should_be(len(pq), 26)
+    should_be((0, 'A'), pq.dequeue())
+    should_be((1, 'B'), pq.dequeue())
+    should_be((2, 'C'), pq.dequeue())
+    should_be(len(pq), 23)
+    should_be((3, 'D'), pq.peek())
     for i, c in [(0, 'A'), (1, 'B'), (2, 'C')]:
         pq.enqueue(i, c)
-    assert list(pq.keys()) == idx_s
-    assert ''.join(pq.values()) == string.ascii_uppercase
+    should_be(list(pq.keys()), idx_s)
+    should_be(''.join(pq.values()), string.ascii_uppercase)
 
     # Test `change` item
     pq[0] = 'ZZZ'
-    assert 0 in pq
-    assert list(pq.keys()) == idx_s[1:] + [0]
-    assert ''.join(pq.values()) == string.ascii_uppercase[1:] + 'ZZZ'
+    should_be(0 in pq, True)
+    should_be(list(pq.keys()), idx_s[1:] + [0])
+    should_be(''.join(pq.values()), string.ascii_uppercase[1:] + 'ZZZ')
     pq[0] = 'A'
-    assert 0 in pq
-    assert list(pq.keys()) == idx_s
-    assert ''.join(pq.values()) == string.ascii_uppercase
+    should_be(0 in pq, True)
+    should_be(list(pq.keys()), idx_s)
+    should_be(''.join(pq.values()), string.ascii_uppercase)
 
     # Test 'delete' item
     i = 0  # removes (0, 'A')
     item = pq[i]  # store value for later
     del pq[i]
-    assert i not in pq
-    assert list(pq.keys()) == idx_s[:i] + idx_s[i+1:]
-    assert ''.join(pq.values()) ==   string.ascii_uppercase[:i]\
-                                   + string.ascii_uppercase[i+1:]
+    should_be(i not in pq, True)
+    should_be(list(pq.keys()), idx_s[:i] + idx_s[i+1:])
+    should_be(''.join(pq.values()),   string.ascii_uppercase[:i]\
+                                    + string.ascii_uppercase[i+1:])
 
     # Re-add item for completeness
     pq[i] = item
 
     # Internal checks
     for i in range(len(pq._pq)-1):
-        assert pq._pq[pq._qp[i]] == i
+        should_be(pq._pq[pq._qp[i]], i)
 
     # Equality checks
     pq1 = IndexPQ(zip(idx, data), kind='min')
     pq2 = IndexPQ(zip(idx, data), kind='min')
-    assert pq1 == pq2
+    should_be(pq1, pq2)
     del pq1, pq2
 
     # Copy check
     pq_copy = pq.copy()
-    assert pq_copy == pq
+    should_be(pq_copy, pq)
 
     pq_fromkeys = pq.fromkeys(idx)
 
-    print('All tests passed.')
+    # Summary
+    if fails > 0:
+        print(f"{fails}/{tests} tests failed")
+    else:
+        print(f"All {tests} tests passed!")
+
 
 #==============================================================================
 #==============================================================================
