b586c1da5458ba2d3d08588471c60c74c64382cc
Bernard Roesler <bernard.roesler@gmail.com>
2021-02-24 18:57:50 -0500

    Make plot in BST study.

--- algs/exercises/bst_study.py
+++ algs/exercises/bst_study.py
@@ -19,20 +19,26 @@ r"""
 """
 # =============================================================================
 
+import pickle
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
 
-from matplotlib.gridspec import GridSpec
+from pathlib import Path
 from tqdm import tqdm
 
-from algs.search import BST_nr
+from algs.search import BST
+
+SAVE_FIGS = True
+FORCE_UPDATE = False
+PICKLE_FILE = Path('./pkl/bst_compares_1e4.pkl')
 
 
 def bst_avg_compares(N):
-    """Theoretical value of average number of compares in a BST.
-    
+    r"""Theoretical value of average number of compares in a BST.
+
     From theory, the value is given by::
 
         2 log N + 2 \gamma - 3 \approx 1.39 log N - 1.85
@@ -49,51 +55,131 @@ def bst_avg_compares(N):
     result : float
         Expected average number of compares per search in a BST of size `N`.
     """
-    return 2*np.log(N) + 2*np.euler_gamma - 3
+    return 2*np.log2(N) + 2*np.euler_gamma - 3
 
 
 def bst_height(N):
     """Theoretical height of BST of size `N`."""
-    return 2.99 * np.log(N)
+    return 2.99 * np.log2(N)
 
 
 # Seed the rng for consistency
 rng = np.random.default_rng(seed=565656)
 
-N_trials = 100
-# Ns = [int(x) for x in [1e4, 1e5, 1e6]]
-# Ns = [int(x) for x in [1e2, 1e3, 1e4]]
-Ns = [int(x) for x in [1e3, 1e4, 1e5]]
+N_trials = 1000
+# ops = [int(x) for x in [1e2, 1e3, 1e4]]  # number of set operations
+ops = [int(x) for x in np.logspace(2, 4)]
+M = len(ops)
 
-# Insert N random (with replacement) keys into an initially empty tree
-cols = pd.MultiIndex.from_product([
-        ['compares', 'height'],
-        ['Experiment', 'Theory'],
-        ['Avg', 'Std'],
-    ])
-df = pd.DataFrame(index=Ns, columns=cols)
+if FORCE_UPDATE or not PICKLE_FILE.exists():
+    # Insert N random (with replacement) keys into an initially empty tree
+    cols = pd.MultiIndex.from_product([
+            ['compares', 'height', 'path lengths'],
+            ['Experiment', 'Theory'],
+            ['Avg', 'Std'],
+        ])
+    df = pd.DataFrame(index=ops, columns=cols)
 
-# Run the experiments
-comps = np.empty(N_trials)      # total number of compares to build table
-heights = np.empty(N_trials)    # height of tree after building
-for N in Ns:
-    print(f"N = {N}...")
-    for i in tqdm(range(N_trials)):
-        st = BST_nr()
-        tot_comp = 0
-        for k in rng.integers(N, size=N):
-            st[k] = 1  # set a arbitrary value
-            tot_comp += st._cost
-        comps[i] = tot_comp / N  # cost of building tree of size N
-        heights[i] = st.height   # height of tree of size N
+    # Run the experiments
+    comps   = np.empty((N_trials, M))
+    heights = np.empty((N_trials, M))
+    ipls    = np.empty((N_trials, M))
+    for j, N in enumerate(ops):
+        print(f"N = {N}...")
+        for i in tqdm(range(N_trials)):
+            st = BST()
+            tot_comp = 0
+            for k in rng.integers(N, size=N):
+                st[k] = 1  # set a arbitrary value
+                tot_comp += st._cost
 
-    df.loc[N, ('compares', 'Experiment')] = comps.mean(), comps.std()
-    df.loc[N, ('compares', 'Theory')]     = bst_avg_compares(N), None
-    df.loc[N, ('height', 'Experiment')]   = heights.mean(), heights.std()
-    df.loc[N, ('height', 'Theory')]       = bst_height(N), None
+            # Store stats for averages
+            comps[i, j] = tot_comp / N  # cost of building tree of size N
+            heights[i, j] = st.height   # height of tree of size N
+            ipls[i, j] = st.internal_path_length / N + 1
 
-df.to_pickle('pkl/bst_compares.pkl')
-print(df)
+        df.loc[N, ('compares', 'Experiment')] = comps[:, j].mean(), comps[:, j].std()
+        df.loc[N, ('compares', 'Theory')]     = bst_avg_compares(N), None
+        df.loc[N, ('height', 'Experiment')]   = heights[:, j].mean(), heights[:, j].std()
+        df.loc[N, ('height', 'Theory')]       = bst_height(N), None
+        df.loc[N, ('path lengths', 'Experiment')] = ipls[:, j].mean(), ipls[:, j].std()
+        df.loc[N, ('path lengths', 'Theory')]     = bst_avg_compares(N), None
+
+    with open(PICKLE_FILE, 'wb') as fp:
+        pickle.dump((comps, heights, ipls, df), fp)
+
+else:
+    # read data from file
+    with open(PICKLE_FILE, 'rb') as fp:
+        (comps, heights, ipls, df) = pickle.load(fp)
+
+# print(df)
+
+# -----------------------------------------------------------------------------
+#         Make Plots
+# -----------------------------------------------------------------------------
+# Organize the data for plotting
+tf = pd.DataFrame(data=ipls, columns=ops)
+tf.index.name = 'trial'
+tf.columns.name = 'N'
+tf = tf.melt(ignore_index=False).reset_index()
+g = tf.drop('trial', axis=1).groupby('N').agg(['mean', 'std'])
+
+# Theory curve
+x = np.logspace(np.log10(min(ops)), np.log10(max(ops)))
+theory_avg_ipl = 1.39 * np.log2(x) - 1.85
+
+fig = plt.figure(1, clear=True)
+ax = fig.add_subplot()
+
+# TODO 
+#   * `x_jitter` argument does nothing (per seaborn docs)
+#   * would like to plot std at each N
+#   * make this plot manually?
+# FIXME Why is experimental data ~ 60% of expected height?
+#   * plot on p 423 in book is actually *compares* not *path length*. Compares
+#   will be larger than path length because there are 2 compares for each
+#   right-ward movement or found key, vs a single edge in the path.
+
+# Plot the runtime distributions
+sns.scatterplot(ax=ax, data=tf, x='N', y='value',
+                color=0.5*np.ones(3), x_jitter=1.0, alpha=0.25)
+
+# Plot the means and stds of each group
+sns.scatterplot(ax=ax, data=g['value'], x='N', y='mean',
+                color='k', marker='d', s=50)
+
+ax.plot(x, theory_avg_ipl, color='C3', ls='-', marker='.')
+
+# Place labels on axis (like ticklabels)
+ax.set_xlabel('operations')
+ax.xaxis.set_label_coords((np.max(ops) - np.min(ops))/2, 0,
+                           transform=ax.xaxis.get_ticklabels()[0].get_transform())
+
+ax.set_ylabel('compares')
+ax.yaxis.set_label_coords(0, np.max(ipls)/2,
+                          transform=ax.yaxis.get_ticklabels()[0].get_transform())
+
+ax.annotate(rf"$\leftarrow$ {theory_avg_ipl[-1]:.0f}",
+            xy=(max(ops) + 100, theory_avg_ipl[-1]),
+            ha='left', va='center', color='C3')
+
+ax.xaxis.label.set_color('C3')
+ax.yaxis.label.set_color('C3')
+# ax.set_xlim([0, max(ops)])
+# ax.set_ylim([0, round(np.max(ipls))])
+ax.set_xticks([min(ops), max(ops)])
+ax.set_yticks([0, round(np.max(ipls))])
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+
+fig.tight_layout()
+
+if SAVE_FIGS:
+    figname = Path(f"./figures/BST_avg_length.pdf")
+    fig.savefig(figname)
+
+plt.show()
 
 # =============================================================================
 # =============================================================================
