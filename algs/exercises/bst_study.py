333d2a818eab17b828b3b61a4d3bd23f552527ae
Bernard Roesler <bernard.roesler@gmail.com>
2021-02-24 21:09:11 -0500

    Fix rng to **not** replace integer keys. Theory matches!

--- algs/exercises/bst_study.py
+++ algs/exercises/bst_study.py
@@ -27,13 +27,15 @@ import pandas as pd
 import seaborn as sns
 
 from pathlib import Path
+from scipy.optimize import curve_fit
 from tqdm import tqdm
 
 from algs.search import BST
 
 SAVE_FIGS = True
-FORCE_UPDATE = False
+FORCE_UPDATE = True
 PICKLE_FILE = Path('./pkl/bst_compares_1e4.pkl')
+# PICKLE_FILE = Path('./pkl/bst_compares_tiny.pkl')
 
 
 def bst_avg_compares(N):
@@ -67,18 +69,18 @@ def bst_height(N):
 rng = np.random.default_rng(seed=565656)
 
 N_trials = 1000
-# ops = [int(x) for x in [1e2, 1e3, 1e4]]  # number of set operations
+# ops = [int(x) for x in [1e2, 1e3, 1e4]]
 ops = [int(x) for x in np.logspace(2, 4)]
 M = len(ops)
 
 if FORCE_UPDATE or not PICKLE_FILE.exists():
     # Insert N random (with replacement) keys into an initially empty tree
-    cols = pd.MultiIndex.from_product([
-            ['compares', 'height', 'path lengths'],
-            ['Experiment', 'Theory'],
-            ['Avg', 'Std'],
-        ])
-    df = pd.DataFrame(index=ops, columns=cols)
+    # cols = pd.MultiIndex.from_product([
+    #         ['compares', 'height', 'path lengths'],
+    #         ['Experiment', 'Theory'],
+    #         ['Avg', 'Std'],
+    #     ])
+    # df = pd.DataFrame(index=ops, columns=cols)
 
     # Run the experiments
     comps   = np.empty((N_trials, M))
@@ -87,10 +89,11 @@ if FORCE_UPDATE or not PICKLE_FILE.exists():
     for j, N in enumerate(ops):
         print(f"N = {N}...")
         for i in tqdm(range(N_trials)):
+            # st = BST.fromkeys(rng.choice(N, size=N, replace=False))
             st = BST()
             tot_comp = 0
-            for k in rng.integers(N, size=N):
-                st[k] = 1  # set a arbitrary value
+            for k in rng.choice(N, size=N, replace=False):
+                st[k] = 1  # set an arbitrary value
                 tot_comp += st._cost
 
             # Store stats for averages
@@ -98,41 +101,67 @@ if FORCE_UPDATE or not PICKLE_FILE.exists():
             heights[i, j] = st.height   # height of tree of size N
             ipls[i, j] = st.internal_path_length / N + 1
 
-        df.loc[N, ('compares', 'Experiment')] = comps[:, j].mean(), comps[:, j].std()
-        df.loc[N, ('compares', 'Theory')]     = bst_avg_compares(N), None
-        df.loc[N, ('height', 'Experiment')]   = heights[:, j].mean(), heights[:, j].std()
-        df.loc[N, ('height', 'Theory')]       = bst_height(N), None
-        df.loc[N, ('path lengths', 'Experiment')] = ipls[:, j].mean(), ipls[:, j].std()
-        df.loc[N, ('path lengths', 'Theory')]     = bst_avg_compares(N), None
+        # Prep summary table
+        # TODO combine matrices into one big df and use that instead.
+        # df.loc[N, ('compares', 'Experiment')] = comps[:, j].mean(), comps[:, j].std()
+        # df.loc[N, ('compares', 'Theory')]     = bst_avg_compares(N), None
+        # df.loc[N, ('height', 'Experiment')]   = heights[:, j].mean(), heights[:, j].std()
+        # df.loc[N, ('height', 'Theory')]       = bst_height(N), None
+        # df.loc[N, ('path lengths', 'Experiment')] = ipls[:, j].mean(), ipls[:, j].std()
+        # df.loc[N, ('path lengths', 'Theory')]     = bst_avg_compares(N), None
 
     with open(PICKLE_FILE, 'wb') as fp:
-        pickle.dump((comps, heights, ipls, df), fp)
+        pickle.dump((comps, heights, ipls), fp)
 
 else:
     # read data from file
     with open(PICKLE_FILE, 'rb') as fp:
-        (comps, heights, ipls, df) = pickle.load(fp)
+        (comps, heights, ipls) = pickle.load(fp)
 
 # print(df)
 
 # -----------------------------------------------------------------------------
-#         Make Plots
+#         Organize the data for plotting
 # -----------------------------------------------------------------------------
-# Organize the data for plotting
-tf = pd.DataFrame(data=ipls, columns=ops)
+data = ipls
+col_name = 'ipl'
+
+tf = pd.DataFrame(data=data, columns=ops)
 tf.index.name = 'trial'
 tf.columns.name = 'N'
-tf = tf.melt(ignore_index=False).reset_index()
-g = tf.drop('trial', axis=1).groupby('N').agg(['mean', 'std'])
+tf = (tf.melt(ignore_index=False)
+        .reset_index()
+        .rename(columns={'value': col_name})
+      )
+
+# Aggregate the meand and std
+g = (tf.drop('trial', axis=1)
+       .groupby('N')
+       .agg(['mean', 'std'])
+       .droplevel(0, axis=1)
+     )
 
 # Theory curve
 x = np.logspace(np.log10(min(ops)), np.log10(max(ops)))
-theory_avg_ipl = 1.39 * np.log2(x) - 1.85
+theory_avg_ipl = bst_avg_compares(x)
+approx_avg_ipl = 1.39 * np.log2(x) - 1.85
 
+
+# Fit curve to data
+def func(x, a, b):
+    return a * np.log2(x) + b
+
+
+popt, pcov = curve_fit(func, g.index, g['mean'])
+print(popt)
+
+# -----------------------------------------------------------------------------
+#         Make Plots
+# -----------------------------------------------------------------------------
 fig = plt.figure(1, clear=True)
 ax = fig.add_subplot()
 
-# TODO 
+# TODO
 #   * `x_jitter` argument does nothing (per seaborn docs)
 #   * would like to plot std at each N
 #   * make this plot manually?
@@ -142,19 +171,23 @@ ax = fig.add_subplot()
 #   right-ward movement or found key, vs a single edge in the path.
 
 # Plot the runtime distributions
-sns.scatterplot(ax=ax, data=tf, x='N', y='value',
+sns.scatterplot(ax=ax, data=tf, x='N', y=col_name,
                 color=0.5*np.ones(3), x_jitter=1.0, alpha=0.25)
 
 # Plot the means and stds of each group
-sns.scatterplot(ax=ax, data=g['value'], x='N', y='mean',
-                color='k', marker='d', s=50)
+sns.scatterplot(ax=ax, data=g, x='N', y='mean',
+                color='k', marker='d', s=50, zorder=9)
 
-ax.plot(x, theory_avg_ipl, color='C3', ls='-', marker='.')
+ax.plot(x, func(x, *popt), color='k', ls='-', label='fit')
+ax.plot(x, approx_avg_ipl, color='C3', ls='-', label=r'$1.39 \lg N - 1.85$')
+ax.plot(x, theory_avg_ipl, color='C3', ls='-.', label=r'$2 \lg N + 2\gamma - 3$')
+
+ax.legend(loc='lower right')
 
 # Place labels on axis (like ticklabels)
 ax.set_xlabel('operations')
 ax.xaxis.set_label_coords((np.max(ops) - np.min(ops))/2, 0,
-                           transform=ax.xaxis.get_ticklabels()[0].get_transform())
+                          transform=ax.xaxis.get_ticklabels()[0].get_transform())
 
 ax.set_ylabel('compares')
 ax.yaxis.set_label_coords(0, np.max(ipls)/2,
@@ -164,19 +197,23 @@ ax.annotate(rf"$\leftarrow$ {theory_avg_ipl[-1]:.0f}",
             xy=(max(ops) + 100, theory_avg_ipl[-1]),
             ha='left', va='center', color='C3')
 
+ax.annotate(rf"$\leftarrow$ {approx_avg_ipl[-1]:.0f}",
+            xy=(max(ops) + 100, approx_avg_ipl[-1]),
+            ha='left', va='center', color='C3')
+
 ax.xaxis.label.set_color('C3')
 ax.yaxis.label.set_color('C3')
 # ax.set_xlim([0, max(ops)])
 # ax.set_ylim([0, round(np.max(ipls))])
 ax.set_xticks([min(ops), max(ops)])
-ax.set_yticks([0, round(np.max(ipls))])
+ax.set_yticks([0, round(np.max(ipls) / 10) * 10])
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 
 fig.tight_layout()
 
 if SAVE_FIGS:
-    figname = Path(f"./figures/BST_avg_length.pdf")
+    figname = Path('./figures/BST_avg_length.pdf')
     fig.savefig(figname)
 
 plt.show()
