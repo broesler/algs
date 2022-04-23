2b1db4c0d26fd26be1a698fbf521b7da2b9af23a
Bernard Roesler <bernard.roesler@gmail.com>
2022-04-23 00:25:07 -0400

    Implement keys() and delete().

--- algs/search/hash.py
+++ algs/search/hash.py
@@ -9,7 +9,10 @@
 """
 # =============================================================================
 
+from algs.basics import Queue as _Queue
 from algs.search.table import SequentialSearchST
+
+__all__ = ['SeparateChainingHashST']
 
 
 class SeparateChainingHashST():
@@ -29,7 +32,7 @@ class SeparateChainingHashST():
     is_empty : bool
         True if `size == 0`.
     """
-    def __init__(self, items=None, M=7):
+    def __init__(self, items=None, M=7, cache=False):
         self.M = M
         self.size = 0
         items = items or []  # must be iterable
@@ -47,34 +50,101 @@ class SeparateChainingHashST():
     def is_empty(self):
         return self.size == 0
 
-    def _hash(self, key):
+    def _hash(self, k):
         """Modular hashing using Python's built-in `hash` function."""
-        # TODO `hash` is "salted" each time python is run for security reasons,
+        # NOTE `hash` is "salted" each time python is run for security reasons,
         # so results are non-deterministic. Manual override or set seed?
-        return hash(key) % self.M
+        return hash(k) % self.M
 
-    def __getitem__(self, key):
+    def __getitem__(self, k):
         """Return the value associated with the given key `k`."""
-        return self.st[self._hash(key)][key]
+        return self.st[self._hash(k)][k]
 
-    def __setitem__(self, key, val):
+    def __setitem__(self, k, v):
         """Insert a new value `v` associated with key `k`.
         If `k` is in the table, change its value to `v`."""
-        self.st[self._hash(key)][key] = val
+        self.st[self._hash(k)][k] = v
         self.size += 1
 
+    # Exercise 3.4.9
+    def __delitem__(self, k):
+        """Delete the item associated with `k`.
+
+        Raises
+        ------
+        KeyError
+            If `k` is not in the table.
+        """
+        del self.st[self._hash(k)][k]
+
+    def __str__(self):
+        return str(st.st)
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__}: {self.__str__()}>"
+
+    # -------------------------------------------------------------------------
+    #         Iterator functions
+    # -------------------------------------------------------------------------
     # Exercise 3.4.19
+    _docstring = """Return an in-order iterator over the {rtype}`.
+
+    Returns
+    -------
+    q : iterator
+        iterator over the {rtype}.
+    """
+
     def keys(self):
-        pass
+        return self._make_iterator(rtype='keys')(self)
+
+    def values(self):
+        return self._make_iterator(rtype='values')(self)
+
+    def items(self):
+        return self._make_iterator(rtype='items')(self)
+
+    keys.__doc__   = _docstring.format(rtype='keys')
+    values.__doc__ = _docstring.format(rtype='values')
+    items.__doc__  = _docstring.format(rtype='items')
+
+    def __iter__(self):
+        """Return an iterator of all of the keys in the table."""
+        yield from self.keys()
+
+    # -------------------------------------------------------------------------
+    #         Private API
+    # -------------------------------------------------------------------------
+    def _make_iterator(self, rtype):
+        """Return an iterator over all of the items in the table."""
+        def iterator(self, lo=None, hi=None):
+            """Iterate over each symbol table."""
+            q = list()
+            for t in self.st:
+                q.extend(t.keys() if rtype == 'keys' else
+                         (t.values() if rtype == 'values' else t.items()))
+            return q
+        return iterator
 
 
 # -----------------------------------------------------------------------------
 #         Run tests
 # -----------------------------------------------------------------------------
 if __name__ == '__main__':
+    # Exercise 3.4.1
     keys = 'EASYQUTION'
     items = [(c, i) for i, c in enumerate(keys)]
-    st = SeparateChainingHashST(items)
+    st = SeparateChainingHashST(M=5)
+
+    # Override _hash function with custom function
+    def __hash(self, k):
+        return 11*(ord(k) - ord('A') + 1) % self.M
+
+    SeparateChainingHashST._hash = __hash    # class patching of all instances
+    # import types
+    # st._hash = types.MethodType(__hash, st)  # instance patching
+    for k, v in items:
+        st[k] = v
 
 # =============================================================================
 # =============================================================================
