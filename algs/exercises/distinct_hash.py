e65bb6b495c88670f2fbf12e2c4c058c3e220b5f
Bernard Roesler <bernard.roesler@gmail.com>
2022-04-26 00:31:11 -0400

    Create free hash slots plot.

---
+++ algs/exercises/distinct_hash.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# =============================================================================
+#     File: distinct_hash.py
+#  Created: 2022-04-25 23:57
+#   Author: Bernie Roesler
+#
+"""
+  Description:
+"""
+# =============================================================================
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from duplicates import count_distinct
+from algs.search.hash import SeparateChainingHashST
+
+rng = np.random.default_rng(seed=56)
+
+# Distinct: M*P
+# Duplicates: N - M*P
+# Free: M - M*P == M*(1 - P)
+
+M = 97  # choose a prime
+st = SeparateChainingHashST(M=M)
+
+Ns = np.r_[[10**e for e in range(0, 7)]]
+ds = np.zeros(len(Ns))
+for i, n in enumerate(Ns):
+    keys = rng.random(size=n)  # floats
+    ints = np.r_[[st._hash(k) for k in keys]]
+    ds[i] = count_distinct(ints)
+
+free_slots = M - ds
+df = (pd.DataFrame(data=np.c_[Ns/M, free_slots],
+                  index=Ns,
+                  columns=['α', 'free'])
+        .astype({'free': int})
+      )
+print(df)
+
+# ----------------------------------------------------------------------------- 
+#         Plots
+# -----------------------------------------------------------------------------
+N = np.linspace(0, 5*M)
+α = N / M
+P = (1 - np.exp(-α))
+# expect = M * (1 - P)
+
+fig = plt.figure(1, clear=True, constrained_layout=True)
+ax = fig.add_subplot()
+# Asymptotes
+ax.plot(α, α - 1, 'k--', lw=1)
+ax.axhline(1, c='k', ls='--', lw=1)
+# Exponentials
+ax.plot(α, P, label=r'Distinct = $M(1 - e^{-\alpha})$')
+ax.plot(α, α - P, label=r'Duplicate = $N - M(1 - e^{-\alpha})$')
+ax.plot(α, 1 - P, label=r'Free Slots = $Me^{-\alpha}$')
+ax.set(xlabel=r'$\alpha = \dfrac{N}{M}$',
+       ylabel='Fraction of (Distinct/Duplicate) vs. M slots')
+ax.set_xlim(left=0)
+ax.set_ylim(bottom=0)
+ax.legend()
+
+plt.show()
+
+# =============================================================================
+# =============================================================================
--- algs/exercises/duplicates.py
+++ algs/exercises/duplicates.py
@@ -21,12 +21,10 @@ import numpy as np
 import scipy.linalg as la
 import pandas as pd
 
-from algs.sort import is_sorted
-
 
 def count_duplicates(x):
     """Count the number of duplicate entries in a sorted array."""
-    assert is_sorted(x)
+    x = np.sort(np.asarray(x))
     dups = 0
     for i in range(len(x)-1):
         if x[i+1] == x[i]:
@@ -36,7 +34,7 @@ def count_duplicates(x):
 
 def count_distinct(x):
     """Count the number of distinct entries in a sorted array."""
-    assert is_sorted(x)
+    x = np.sort(np.asarray(x))
     res = 1
     for i in range(len(x)-1):
         if x[i+1] != x[i]:
@@ -44,66 +42,46 @@ def count_distinct(x):
     return res
 
 
-rng = np.random.default_rng(seed=56)
+if __name__ == '__main__':
+    rng = np.random.default_rng(seed=56)
 
-T = 10                                    # trials
-Ns = np.r_[[10**e for e in range(3, 6)]]  # number of integers
-alphas = np.r_[2.0, 1.0, 0.5]             # fractions of N
+    T = 10                                    # trials
+    Ns = np.r_[[10**e for e in range(3, 6)]]  # number of integers
+    alphas = np.r_[2.0, 1.0, 0.5]             # fractions of N
 
-cols = pd.MultiIndex.from_product((Ns, alphas, ['dups_a', 'distinct_a']))
-cols.names = ['N', 'α', 'kind']
-data = np.zeros((T, len(Ns)*len(alphas)*2), dtype=int)
-df = pd.DataFrame(index=range(T), columns=cols, data=data)
-df.index.name ='T'
+    cols = pd.MultiIndex.from_product((Ns, alphas,
+                                    ['duplicate', 'distinct'],
+                                    ['actual', 'expect']))
+    cols.names = ['N', 'α', '', '']
+    data = np.zeros((T, len(cols)), dtype=int)
+    df = pd.DataFrame(index=range(T), columns=cols, data=data)
+    df.index.name ='T'
 
-for N in Ns:
-    for α in alphas:
-        M = N / α   # number of integers
-        ints = rng.integers(M, size=(T, N))
-        ints = np.sort(ints, axis=1)
-        for t in range(T):
-            df.loc[t, (N, α, 'dups_a')] = count_duplicates(ints[t])
-            df.loc[t, (N, α, 'distinct_a')] = count_distinct(ints[t])
+    for N in Ns:
+        for α in alphas:
+            M = N / α   # number of integers
+            ints = rng.integers(M, size=(T, N))
+            ints = np.sort(ints, axis=1)
+            for t in range(T):
+                df.loc[t, (N, α, 'duplicate', 'actual')] = count_duplicates(ints[t])
+                df.loc[t, (N, α, 'distinct', 'actual')] = count_distinct(ints[t])
 
-# Average over the trials
-df = (df.mean()
-        .unstack()
-        .astype(int)
-        .reset_index()
-      )
-M = (df['N'] / df['α']).astype(int)
-df['expect_distinct'] = (M * (1 - np.exp(-df['α']))).astype(int)
-df['expect_dups'] = df['N'] - df['distinct_a']
-# df['a/e'] = df['actual'] / df['expect']
-df = df.set_index(['N', 'α'])
-df = df[df.columns[[0, 2, 1, 3]]]  # swap order
+    # Average over the trials
+    df = (df.mean()
+            .unstack([-2, -1])  # move duplicate/distinct, actual/expect to cols
+            .reset_index()      # move N, α to cols for math ops
+        )
 
+    # Compute expected values
+    M = df['N'] // df['α']
+    df[('distinct', 'expect')] = M * (1 - np.exp(-df['α']))  # probability theory
+    df[('duplicate', 'expect')] = df['N'] - df[('distinct', 'expect')]
+    assert np.allclose((df['distinct'] + df['duplicate']).values.T - df['N'].values, 0)
 
-# ----------------------------------------------------------------------------- 
-#         Plots
-# -----------------------------------------------------------------------------
-M = 100
-N = np.linspace(0, 5*M)
-α = N / M
-P = (1 - np.exp(-α))
-D = M * P
+    # Clean-up for presentation
+    df = df.set_index(['N', 'α']).round().astype(int)
 
-fig = plt.figure(1, clear=True, constrained_layout=True)
-ax = fig.add_subplot()
-# Asymptotes
-ax.plot(α, α - 1, 'k--', lw=1)
-ax.axhline(1, c='k', ls='--', lw=1)
-# Exponentials
-ax.plot(α, P, label=r'Distinct = $M(1 - e^{-\alpha})$')
-ax.plot(α, α - P, label=r'Duplicate = $N - M(1 - e^{-\alpha})$')
-ax.plot(α, 1 - P, label=r'Free Slots = $Me^{-\alpha}$')
-ax.set(xlabel=r'$\alpha = \dfrac{N}{M}$',
-       ylabel='Fraction of (Distinct/Duplicate) vs. M slots')
-ax.set_xlim(left=0)
-ax.set_ylim(bottom=0)
-ax.legend()
-
-plt.show()
+    print(df)
 
 # =============================================================================
 # =============================================================================
