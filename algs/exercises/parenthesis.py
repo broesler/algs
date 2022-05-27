f5c7dd4359d478dee9d5d9fa9960f07877465e9a
Bernard Roesler <bernard.roesler@gmail.com>
2022-05-27 13:54:42 -0400

    Implement parentheses checking.

---
+++ algs/exercises/parenthesis.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# =============================================================================
+#     File: parenthesis.py
+#  Created: 2022-05-27 13:42
+#   Author: Bernie Roesler
+#
+"""
+Exercise 1.3.4 Determine if a series of parentheses are in order.
+"""
+# =============================================================================
+
+from algs.basics import Stack
+
+a = list('[()]{}{[()()]()}')  # True
+b = list('[(])')              # False
+
+
+def parentheses(a):
+    """Return True if the parentheses in `a` are properly paired."""
+    pairs = dict({'[': ']', '(': ')', '{': '}'})
+    opens = pairs.keys()
+    closes = pairs.values()
+    s = Stack()
+    for c in a:
+        if c in opens:
+            s.push(c)
+        elif c in closes:
+            x = s.pop()
+            if c != pairs[x]:
+                return False
+    return True
+
+
+assert parentheses(a)
+assert not parentheses(b)
+
+# =============================================================================
+# =============================================================================
