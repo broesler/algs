4e7275d5d185a0c7d9a895153f012e4ab2a2676e
Bernard Roesler <bernard.roesler@gmail.com>
2022-05-30 18:59:14 -0400

    Assert only one group.

--- algs/exercises/random_grid_plot.py
+++ algs/exercises/random_grid_plot.py
@@ -17,14 +17,15 @@ from algs.unionfind import (full_grid, random_grid, plot_grid,
                             WeightedQuickUnionUF)
 
 # Generate a random grid
-N = 25  # 625 sites
-f = full_grid(N)
-g = random_grid(N)
+N = 25              # 625 sites
+f = full_grid(N)    # grid with all connections
+g = random_grid(N)  # grid with random connections but only one group
 
 # Connect components using UnionFind
 uf = WeightedQuickUnionUF(N*N, items=g, store=True)
+assert uf.count == 1
 
-# Plot
+# Plot the full grid with random connections overlaid
 fig, ax = plt.subplots(num=1, clear=True, tight_layout=True)
 fig.set_size_inches((8, 8), forward=True)
 plot_grid(N, f, label_nodes=False, fig=fig, ax=ax, alpha=0.1)
