a3b97995d833aa7e3874defa6ad7acaf908e67ef
Bernard Roesler <bernard.roesler@gmail.com>
2022-06-08 12:59:43 -0400

    Import specials.

--- algs/__init__.py
+++ algs/__init__.py
@@ -3,6 +3,7 @@ from .basics import *
 from .unionfind import *
 from .sort import *
 from .search import *
+from .special import *
 from .graph import *
 
 __all__ = [s for s in dir() if not s.startswith('_')]
