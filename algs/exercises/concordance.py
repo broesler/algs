21e7294d1004bb3576cd23f11fcdcf70589e0f30
Bernard Roesler <bernard.roesler@gmail.com>
2022-06-09 16:23:10 -0400

    Highlight word in context.

--- algs/exercises/concordance.py
+++ algs/exercises/concordance.py
@@ -66,28 +66,43 @@ class Concordance():
                 for j, word in enumerate(words):
                     if len(word) >= minlen:
                         lo = max([0, j - self.offset])
-                        hi = min([j + self.offset, len(line)])
+                        hi = min([j + self.offset, len(words)])
                         # Add a node to the multi-valued index
-                        context = ' '.join(words[lo:hi+1])
+                        context = ' '.join(words[lo:j]
+                                           + [f"*{words[j]}*"]  # highlight!
+                                           + words[j+1:hi+1])
                         self.st[word] = self._Node(i, j, context)
 
     def query(self, k):
         """Return the information associated with the given word `k`."""
         return self.st.get_all(k)
 
+    def print_query(self, k):
+        """Print the information returned by querying `k`."""
+        print(k)
+        for p in self.query(k):
+            print(' ', p.context)
+
 
 class InvertedConcordance():
     """Invert a concordance by placing words at their line and index
     locations."""
 
-    def __init__(self, c):
+    def __init__(self, c, verbose=True):
         self.lines = RedBlackBST()  # ordered index of line numbers
-        for k, vals in c.st.items():
+        iters = c.st.items()
+        if verbose:
+            iters = tqdm(iters)
+        for k, vals in iters:
             for v in vals:
                 if v.i not in self.lines:
                     self.lines[v.i] = RedBlackBST()
                 self.lines[v.i][v.j] = k
 
+    # NOTE this concatenation is SLOW. There is no way to limit the
+    # `self.lines.values()` computation externally. We could either make it
+    # a generator under-the-hood, or make higher-level generators to only
+    # concatenate words/lines as needed.
     def concat(self):
         """Concatenate the words in each line, then all of the lines."""
         return '\n'.join([' '.join(x.values()) for x in self.lines.values()])
@@ -96,17 +111,15 @@ class InvertedConcordance():
 if __name__ == "__main__":
     import pydoc
 
-    file = Path('../data/tiny_tale.txt')
-    # file = Path('../data/tale.txt')
+    # file = Path('../data/tiny_tale.txt')
+    file = Path('../data/tale.txt')
 
     c = Concordance(file, verbose=True)
     q = 'times'
-    print(f"----- {repr(q)}? -----")
-    print(c.query(q))
+    c.print_query(q)
 
     print('----- Inverted -----')
-    t = InvertedConcordance(c)
-    # print(t.concat())
+    t = InvertedConcordance(c, verbose=True)
     pydoc.pager(t.concat())
 
 # =============================================================================
