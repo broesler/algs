0594abc1ba9e8cdc79da7257855d1c3f49fc8b3d
Bernard Roesler <bernard.roesler@gmail.com>
2022-06-12 23:39:28 -0400

    Implement SparseMatrix. A @ I is broken.

--- algs/search/sparse.py
+++ algs/search/sparse.py
@@ -9,11 +9,14 @@ Sparse vector and matrix classes using symbol tables.
 """
 # =============================================================================
 
+import numpy as np
 from operator import iadd, isub, imul, itruediv
 
 from algs.search.hash import HashST
+from algs.search.set import MathSet
 
 
+# TODO `.todense()` method that returns an `np.ndarray`.
 class SparseVector:
     """Implements a sparse vector.
 
@@ -40,22 +43,30 @@ class SparseVector:
         """Return the number of elements in the vector."""
         return self._st.size()
 
+    @property
+    def shape(self):
+        """Return the number of elements in the vector."""
+        return (self.N,)
+
     def __setitem__(self, i, v):
         """Put value `v` at integer index `i`."""
         try:
             idxs, vals = iter(i), iter(v)
             for idx, val in zip(idxs, vals):
+                assert idx < self.N
                 self._st[int(idx)] = val
         except TypeError:
+            assert 0 <= i < self.N
             self._st[int(i)] = v
 
     def __getitem__(self, i):
         """Get the value from index `i`."""
         i = int(i)
+        assert 0 <= i < self.N
         if i in self._st:
             return self._st[i]
         else:
-            return 0.0
+            return 0
 
     def copy(self):
         """Make a copy."""
@@ -74,7 +85,7 @@ class SparseVector:
         """Return the dot product of this vector with another."""
         if self.N != other.N:
             raise ValueError('dimension mismatch!')
-        s = 0.0
+        s = 0
         for i in self._st.keys():
             s += self[i] * other[i]
         return s
@@ -83,6 +94,7 @@ class SparseVector:
         return self.dot(other)
 
     # Exercise 3.5.16
+    # FIXME does not account for elements in `other` that are not in `self`.
     def __add__(self, other):
         """Return the sum of this vector with another."""
         return self._op(other, op=iadd)
@@ -114,7 +126,11 @@ class SparseVector:
         if self.N != other.N:
             raise ValueError('dimension mismatch!')
         A = self.copy()
-        for i in A._st.keys():
+        if op in [iadd, isub]:
+            iters = set(A._st.keys()).union(set(other._st.keys()))
+        else:
+            iters = A._st.keys()
+        for i in iters:
             A[i] = op(A[i], other[i])
             if np.isclose(A[i], 0, atol=1e-16):
                 del A._st[i]
@@ -134,68 +150,230 @@ class SparseVector:
     def get(self, i):
         return self.__getitem__(i)
 
-    def __repr__(self):
-        return f"<({self.N},) SparseVector with {self.size} stored elements."""
-
     def __str__(self):
         return '\n'.join([f"({i},)\t{v}" for i, v in self])
+
+    def __repr__(self):
+        return f"<({self.N},) {self.__class__.__name__} with {self.size} stored elements."""
 
     # Iterate over coordinates and values
     def __iter__(self):
         yield from self._st.items()
 
+    def todense(self):
+        A = np.zeros((self.N,))
+        for k, v in self:
+            A[k] = v
+        return A
 
-class SparseMatrix:
-    """Implements a sparse matrix."""
+
+# Exercise 3.5.23
+# TODO constructors for row/column vectors
+class SparseMatrix():
+    """Implements a sparse matrix of size `M x N`."""
 
     def __init__(self, shape):
         self.M = shape[0]
         self.N = shape[1]
+        self._size = 0
         self._st = HashST()  # _st represents rows
 
-    def __setitem__(self, i, j, v):
+    @property
+    def size(self):
+        return self._size
+
+    @property
+    def shape(self):
+        """Return the number of elements in the vector."""
+        return (self.M, self.N)
+
+    def copy(self):
+        """Make a copy."""
+        A = self.__class__((self.M, self.N))
+        for (i, j), v in self:
+            A[i, j] = v
+        return A
+
+    def __setitem__(self, k, v):
+        # Parse indices
+        if isinstance(k, int):
+            i, j = k, None
+        else:
+            i, j = k
+        # if not (0 <= i < self.M):
+        #     raise IndexError(f"Cannot index row {i} in matrix of size ({self.M}, {self.N})!")
         if i not in self._st:
             self._st[i] = SparseVector(self.N)
-        self._st[i][j] = v
-
-    def __getitem__(self, i, j):
-        if i in self._st:
-            return self._st[i][j]
+        if j is not None:
+            self._st[i][j] = v
+            self._size += 1
         else:
-            return 0.0
+            self._st[i] = v
+            self._size += v.size
+
+    def __getitem__(self, k):
+        # Parse indices
+        if isinstance(k, int):
+            i, j = k, None
+        else:
+            i, j = k
+        # Return value
+        if i in self._st:
+            if j is not None:
+                return self._st[i][j]
+            else:
+                return self._st[i]
+        else:
+            return 0
+
+    def col(self, j):
+        """Return the `j`th column."""
+        c = SparseVector(self.M)
+        for i in self._st.keys():
+            c[i] = self[i, j]
+        return c
+
+    def transpose(self):
+        A = self.__class__((self.M, self.N))
+        for (i, j), v in self:
+            A[j, i] = v
+        return A
+
+    @property
+    def T(self):
+        return self.transpose()
+
+    def __add__(self, other):
+        if not isinstance(other, SparseMatrix):
+            return NotImplemented
+        if self.shape != other.shape:
+            raise ValueError(f"Cannot add {self.shape} to {other.shape}")
+        A = self.copy()
+        iters = set(self._st.keys()).union(set(other._st.keys()))
+        for i in iters:
+            A._st[i] = self._st[i] + other._st[i]
+        return A
+
+    def dot(self, x):
+        """Return the matrix multiplication of this matrix with another."""
+        if not (isinstance(x, SparseVector) 
+                or isinstance(x, SparseMatrix)):
+            return NotImplemented
+        if self.shape[1] != x.shape[0]:
+            raise ValueError(f"Cannot multiply {self.shape} to {x.shape}")
+        if len(x.shape) > 1:
+            b = SparseMatrix((self.N, x.M))
+            for i in self._st.keys():
+                # FIXME this line calls SparseVector.dot() even if `x` is matrix,
+                # which then computes dot products of A[i] with *rows* of x.
+                b[i] = A[i].dot(x)
+        else:
+            b = SparseVector(self.N)
+            for i in self._st.keys():
+                b[i] = A[i].dot(x)
+        return b
+
+    def __matmul__(self, x):
+        return self.dot(x)
+
+    # # FIXME should return x (1, M) @ self (M, N) -> (1, N)
+    # def __rmatmul__(self, x):
+    #     if isinstance(x, SparseVector):
+    #         return x.dot(self)
+    #     else:
+    #         return NotImplemented
+    #     # return self.dot(x)
+
+    def __str__(self):
+        return '\n'.join([f"({i}, {j})\t{v}" for (i, j), v in self])
+
+    def __repr__(self):
+        return f"<{self.shape} {self.__class__.__name__} with {self.size} stored elements."""
+
+    def _items(self):
+        items = list()
+        for i, row in self._st.items():
+            for j, v in row:
+                items.append(((i, j), v))
+        return items
+
+    def __iter__(self):
+        yield from self._items()
+        
+    def todense(self):
+        A = np.zeros((self.M, self.N))
+        for (i, j), v in self:
+            A[i, j] = v
+        return A
 
 
 if __name__ == "__main__":
-    import numpy as np
     N = 10
     a = SparseVector(N)
     b = SparseVector(N)
     x = np.zeros(10)
-    a[[1, 3, 5]] = [1, 3, 5]
-    b[[1, 3, 5]] = [1, 3, 5]
-    x[[1, 3, 5]] = [1, 9, 25]
-    print(a * b)
+    a[[1, 3, 6, 7]] = [2, 4, 6, 7]
+    b[[1, 3, 6, 8]] = [1, 3, 5, 7]
+    x[[1, 3, 6]] = [2, 12, 30]
+    print('a =', a.todense())
+    print('b =', b.todense())
+    print('a = ')
+    print(a)
+    assert np.allclose((3*a).todense(), np.r_[0, 6, 0, 12, 0, 0, 18, 21, 0, 0])
+    assert np.allclose((3 + a).todense(), np.r_[0, 5, 0, 7, 0, 0, 9, 10, 0, 0])
     print(f"{a @ b = }")
     print(f"{a.dot(b) = }")
-    # assert np.allclose(a * b, x)
-    assert np.isclose(a @ b, 35.0)
-    assert np.isclose(a.dot(b), 35.0)
-    print('a + b =')
-    print(a + b)
-    print('3*a =')
-    print(3 * a)
-    print('3 + a =')
-    print(1 + a)
-    print('a / b =')
-    print(a / b)
+    assert np.allclose((a * b).todense(), x)
+    assert np.isclose(a @ b, 44.0)
+    assert np.isclose(a.dot(b), 44.0)
+    assert np.allclose((a + b).todense(), np.r_[0, 3, 0, 7, 0, 0, 11, 7, 7, 0])
+    assert np.allclose((a - b).todense(), np.r_[0, 1, 0, 1, 0, 0, 1, 7, -7, 0])
+    # print('a / b =')
+    # print(a / b)
+    # Test close to 0
     c = SparseVector(N)
     d = SparseVector(N)
     c[[1, 3, 5]] = [1,  3, 5]
     d[[1, 3, 5]] = [1, -3, 5]
-    print('c + d =')
-    print(c + d)
-    print('c - d =')
-    print(c - d)
+    assert np.allclose((c + d).todense(), np.r_[0, 2, 0, 0, 0, 10, 0, 0, 0, 0])
+    assert np.allclose((c - d).todense(), np.r_[0, 0, 0, 6, 0, 0, 0, 0, 0, 0])
+
+    # Test matrices
+    N = 4
+    I = SparseMatrix((N, N))  # identity
+    A = SparseMatrix((N, N))  # first forward difference
+    for i in range(N):
+        I[i, i] =  1
+        if i > 0:
+            A[i, i-1] = -1
+        A[i, i] = 1
+    print('----- Matrices -----')
+    print(I.todense())
+    print(I)
+    print('A')
+    print(A.todense())
+    print('A.T')
+    print(A.T.todense())
+    print('A + A.T')
+    print((A + A.T).todense())
+    x = SparseVector(N)
+    x[[0, 1, 2, 3]] = [1, 1, 1, 1]
+    print('A @ x')
+    print(A @ x)  # delta[0] vector
+    print((A[0] @ A).todense())  # select first row??
+    print((A @ A[0]).todense())  # select first column
+    print('A @ I')
+    print((A @ I).todense())
+    print('I @ A')
+    print((I @ A).todense())  # FIXME
+    # print('A.T @ A')
+    # K = A.T @ A
+    # # K = A @ A.T
+    # print(K.todense())
+    # # [[1, -1, 0, 0],
+    # #  [-1, 2, -1, 0],
+    # #  [0, -1, 2, -1],
+    # #  [0, 0, -1, 2]]
 
 # =============================================================================
 # =============================================================================
