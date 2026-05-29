6f5813d85b0d67a01c7f2cb32bc245c470d6a20f
Bernard Roesler <bernard.roesler@gmail.com>
2026-05-29 14:11:38 -0400

    MNT: Update style of graph_tests.

--- exercises/graph_tests.py
+++ exercises/graph_tests.py
@@ -1,23 +1,38 @@
 #!/usr/bin/env python3
-#==============================================================================
+# ==============================================================================
 #     File: test_graph.py
 #  Created: 2019-03-03 23:31
 #   Author: Bernie Roesler
-#
-"""
-  Description: Test graph data structures
-"""
-#==============================================================================
+# ==============================================================================
 
-from algs.graph.edgeweighted import *
+"""Test graph data structures."""
+
+from pathlib import Path
+
+from algs.graph.directed import Topological
+from algs.graph.edgeweighted import (
+    AcyclicPath,
+    BreadthFirstSearch,
+    DepthFirstOrder,
+    DepthFirstSearch,
+    Digraph,
+    DirectedCycle,
+)
+
+DATA_PATH = Path(__file__).parent.parent / 'data'
+
 
 def load_graph(filename):
+    """Load a graph from a file."""
     G = Digraph()
-    with open(filename, 'r') as file:
+    with Path(filename).open() as file:
         for i, line in enumerate(file.readlines()):
-            if i == 0: V = int(line.rstrip())
-            if i == 1: E = int(line.rstrip())
-            if i < 2: continue
+            if i == 0:
+                V = int(line.rstrip())
+            if i == 1:
+                E = int(line.rstrip())
+            if i < 2:
+                continue
             nums = line.rstrip().split()
             args = [int(nums[0]), int(nums[1])]
             if len(nums) == 3:
@@ -27,17 +42,18 @@ def load_graph(filename):
     assert G.E == E
     return G
 
+
 # Load test file
 # TODO loop over all test files
-G = load_graph('../data/tinyDG.txt')
-# G = load_graph('../data/mediumDG.txt')
+G = load_graph(DATA_PATH / 'tinyDG.txt')
+# G = load_graph(DATA_PATH / 'mediumDG.txt')
 
 sources = G.roots()
 # assert s == 7  # only for tinyDG.txt
 
 Gr = G.reverse()
 for s in sources:
-    assert Gr[s] == list()  # source has no adjacents in reverse
+    assert Gr[s] == []  # source has no adjacents in reverse
 
 # Test DFS
 dfs = DepthFirstSearch(G, [sources[0]])
@@ -65,14 +81,14 @@ bfs_o = BreadthFirstSearch(G, [s], ordered=True)
 # print('------------ Ordered BFS: ------------')
 # bfs_o.print_paths()
 
-# Test TopologicalOrder
-DAG = load_graph('../data/tinyDAG.txt')
-topo = TopologicalOrder(DAG)
+# Test Topological
+DAG = load_graph(DATA_PATH / 'tinyDAG.txt')
+topo = Topological(DAG)
 assert topo
 print('order: ', topo)
 
 # Test EWD
-EG = load_graph('../data/tinyEWDAG.txt')
+EG = load_graph(DATA_PATH / 'tinyEWDAG.txt')
 s = EG.roots()
 assert len(s) == 1
 ap = AcyclicPath(EG, s[0], kind='max')
@@ -85,5 +101,5 @@ print('----- Min Edge-Weighted DAG -----')
 ap.print_paths()
 assert ap.dist_to(s[0]) == 0.0
 
-#==============================================================================
-#==============================================================================
+# ==============================================================================
+# ==============================================================================
