b34c889638e17fd9af01d7049db3ea69362f4345
Bernard Roesler <bernard.roesler@gmail.com>
2026-05-29 13:48:27 -0400

    MNT: Update graph density plot style.

--- exercises/graph_density_plot.py
+++ exercises/graph_density_plot.py
@@ -3,16 +3,14 @@
 #     File: graph_density_plot.py
 #  Created: 2022-07-21 16:10
 #   Author: Bernie Roesler
-#
-"""
-Draw the graphs on p 520 (sparse vs. dense).
-"""
 # =============================================================================
+
+"""Draw the graphs on p 520 (sparse vs. dense)."""
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-from algs.graph import random_simple_graph, EuclideanGraph
+from algs.graph import EuclideanGraph, random_simple_graph
 
 V = 50
 Es = [200, 1000]
@@ -30,8 +28,15 @@ for g, E, tag in zip(gs, Es, tags):
     G = EuclideanGraph(random_simple_graph(V, E), x, y)
     ax = fig.add_subplot(g)
     G.draw()
-    ax.set_title(rf"{tag} ($E$ = {E})", color='C3',
-                fontsize=9, x=0, ha='left', pad=10, va='bottom')
+    ax.set_title(
+        rf"{tag} ($E$ = {E})",
+        color='C3',
+        fontsize=9,
+        x=0,
+        ha='left',
+        pad=10,
+        va='bottom',
+    )
 
 plt.show()
 
