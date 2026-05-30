891ec6d8ad2713b818aff2b84c01e5f7bbe53a82
Bernard Roesler <bernard.roesler@gmail.com>
2026-05-29 21:39:57 -0400

    MNT: Update timesorts style.

--- exercises/timesorts.py
+++ exercises/timesorts.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python3
-#==============================================================================
+# ==============================================================================
 #     File: test_timesorts.py
 #  Created: 2019-03-21 00:12
 #   Author: Bernie Roesler
-#
-"""
-  Description: Time the run of sorting algorithms.
-"""
-#==============================================================================
+# ==============================================================================
 
-import time
+"""Time the run of sorting algorithms."""
+
 import itertools
-
-import numpy as np
-import matplotlib.pyplot as plt
-
+import time
 from collections import defaultdict
 
-from algs.sort import *
+import matplotlib.pyplot as plt
+import numpy as np
 
-sort_funs = [bubble_sort, insertion_sort, mergesort, mergesort_BU,
-             qsort, heap_sort]
+from algs.sort import (
+    bubble_sort,
+    heap_sort,
+    insertion_sort,
+    is_sorted,
+    mergesort,
+    mergesort_BU,
+    qsort,
+)
+
+sort_funs = [bubble_sort, insertion_sort, mergesort, mergesort_BU, qsort, heap_sort]
 # sort_funs = [qsort0, qsort1, qsort2, qsort]
 
 # Define lengths of input
@@ -29,14 +33,16 @@ vals = np.power(2, np.arange(np.log2(Nmax))).astype(np.int64)
 M = len(vals)
 
 # Massive arrays from which to sample later
-masters = dict()
-masters['random'] = np.random.randint(max(vals), size=max(vals))
+rng = np.random.default_rng(seed=565656)
+masters = {}
+masters['random'] = rng.integers(max(vals), size=max(vals))
 masters['sorted'] = np.array(sorted(masters['random']))
 # masters['reverse'] = np.array(masters['sorted'][::-1])
 # masters['equal'] = np.ones(max(vals))
-masters['binary'] = np.concatenate([np.ones(max(vals//2)), 
-                                    np.zeros(max(vals//2))]).astype(np.int64)
-np.random.shuffle(masters['binary'])
+masters['binary'] = np.concatenate(
+    [np.ones(max(vals // 2)), np.zeros(max(vals // 2))]
+).astype(np.int64)
+rng.shuffle(masters['binary'])
 Ntypes = len(masters)
 
 # Initialize dictionary
@@ -56,27 +62,33 @@ for sort in sort_funs:
             runtimes[name][kind][i, :] = (N, stop - start)
 
 
-#------------------------------------------------------------------------------ 
+# ------------------------------------------------------------------------------
 #        Plots!
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 fig = plt.figure(1, clear=True)
 ax = fig.add_subplot(111)
 
-colors = ax._get_lines.prop_cycler
+# TODO rewrite with pandas and seaborn
+colors = plt.rcParams['axes.prop_cycle']()
 marker = itertools.cycle(('x', 'o', '^', 's', 'd')[:Ntypes])
 
 for sort in sort_funs:
     name = sort.__name__
     color = next(colors)['color']
     for kind in masters:
-        ax.plot(runtimes[name][kind][:, 0], runtimes[name][kind][:, 1],
-                c=color, marker=next(marker), ls='-',
-                label="{}: {}".format(name.replace('_', ' '), kind))
+        ax.plot(
+            runtimes[name][kind][:, 0],
+            runtimes[name][kind][:, 1],
+            c=color,
+            marker=next(marker),
+            ls='-',
+            label=f"{name.replace('_', ' ')}: {kind}",
+        )
 
 ax.set_xscale('log')
 ax.set_yscale('log')
-ax.grid(b=True, which='major', c='k')
-ax.grid(b=True, which='minor')
+ax.grid(which='major', c='k')
+ax.grid(which='minor')
 
 ax.legend()
 ax.set_xlabel('$N$')
@@ -86,5 +98,5 @@ plt.show()
 
 print('done.')
 
-#==============================================================================
-#==============================================================================
+# ==============================================================================
+# ==============================================================================
