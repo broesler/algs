ce9a8d3d574bb48daddc0f3127dbc09fd12dd806
Bernard Roesler <bernard.roesler@gmail.com>
2026-05-29 21:40:48 -0400

    MNT: Update tree_to_list style.

--- exercises/tree_to_list.py
+++ exercises/tree_to_list.py
@@ -3,22 +3,21 @@
 #     File: tree_to_list.py
 #  Created: 2020-02-19 21:44
 #   Author: Bernie Roesler
-#
-"""
-  Description: 
-    **The great tree-list recursion problem.** A binary search tree and
-    a circular doubly linked list are conceptually built from the same type of
-    nodes - a data field and two references to other nodes. Given a binary
-    search tree, rearrange the references so that it becomes a circular
-    doubly-linked list (in sorted order). Nick Parlante describes this as 
-    [one of the neatest recursive pointer problems ever
-    devised](http://cslibrary.stanford.edu/109/TreeListRecursion.html).
-
-    *Hint:* create a circularly linked list A from the left subtree,
-    a circularly linked list B from the right subtree, and make the root a one
-    node circularly linked list. Then merge the three lists.
-"""
 # =============================================================================
+
+"""**The great tree-list recursion problem**.
+
+A binary search tree and a circular doubly linked list are conceptually built
+from the same type of nodes - a data field and two references to other nodes.
+Given a binary search tree, rearrange the references so that it becomes
+a circular doubly-linked list (in sorted order). Nick Parlante describes this
+as [one of the neatest recursive pointer problems ever
+devised](http://cslibrary.stanford.edu/109/TreeListRecursion.html).
+
+*Hint:* create a circularly linked list A from the left subtree, a circularly
+linked list B from the right subtree, and make the root a one node circularly
+linked list. Then merge the three lists.
+"""
 
 import copy
 
@@ -85,7 +84,7 @@ if __name__ == '__main__':
     print("Tree:", t.keys())
     A = tree_to_list(t)
     x = A
-    keys = list()
+    keys = []
     while True:
         keys.append(x.key)
         x = x.right
