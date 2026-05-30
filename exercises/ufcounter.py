13c5717109470f40cb46097ea3ad89569d172605
Bernard Roesler <bernard.roesler@gmail.com>
2026-05-29 21:45:49 -0400

    MNT: Update ufcounter style.

--- exercises/ufcounter.py
+++ exercises/ufcounter.py
@@ -3,14 +3,11 @@
 #     File: ufcounter.py
 #  Created: 2022-05-26 15:56
 #   Author: Bernie Roesler
-#
-"""
-  Description:
-"""
 # =============================================================================
 
-import numpy as np
+"""Define the Union-Find Counter class."""
 
+import numpy as np
 from tqdm import tqdm
 
 from algs.unionfind import read_uf_file
@@ -33,18 +30,32 @@ def cost_plot(oc, ax=None, title='', y_max=0):
     ax : Axes
         The axes in which the plot was made.
     """
-    ax.scatter(oc.ops, oc.cost, color=0.7*np.r_[1, 1, 1], s=1, alpha=0.8)
+    ax.scatter(oc.ops, oc.cost, color=0.7 * np.r_[1, 1, 1], s=1, alpha=0.8)
     ax.scatter(oc.ops, oc.tots, c='C3', s=1, alpha=0.8)
 
     # Label the final average total cost value
-    ax.annotate(rf"{oc.tots[-1]:.0f}",
-                xy=(oc.M, oc.tots[-1]), xycoords='data',
-                xytext=(1.1*oc.M, 1.1*oc.tots[-1]), textcoords='data',
-                ha='center', va='bottom', color='C3',
-                arrowprops=dict(arrowstyle="-|>", color="C3"))
+    ax.annotate(
+        rf"{oc.tots[-1]:.0f}",
+        xy=(oc.M, oc.tots[-1]),
+        xycoords='data',
+        xytext=(1.1 * oc.M, 1.1 * oc.tots[-1]),
+        textcoords='data',
+        ha='center',
+        va='bottom',
+        color='C3',
+        arrowprops={'arrowstyle': "-|>", 'color': "C3"},
+    )
 
-    ax.set_title(title, color='C3', fontweight='bold', fontsize=9,
-                 x=0, ha='left', pad=10, va='bottom')
+    ax.set_title(
+        title,
+        color='C3',
+        fontweight='bold',
+        fontsize=9,
+        x=0,
+        ha='left',
+        pad=10,
+        va='bottom',
+    )
 
     ax.set_xlim((0, oc.M))
 
@@ -64,7 +75,7 @@ def cost_plot(oc, ax=None, title='', y_max=0):
     return ax
 
 
-class UFCounter():
+class UFCounter:
     """Class to count the number of operations in a union-find data type.
 
     Parameters
@@ -91,27 +102,31 @@ class UFCounter():
         A list of the cumulative average number of array accesses per operation
         in `uf`.
     """
+
     def __init__(self, UF, N, **kwargs):
         self.uf = UF(N, **kwargs)
         self.N = N
         self.M = 0
         self.ops = [0]
-        self.cost = list()
-        self.totals = list()
+        self.cost = []
+        self.totals = []
 
     @property
     def tots(self):
+        """Cumulative average total cost per operation."""
         return self.totals / self.ops
 
     @classmethod
     def fromfile(cls, file, UF, **kwargs):
+        """Create a UFCounter from a file containing connections."""
         N, items = read_uf_file(file)
         return cls.fromitems(items, UF, N, **kwargs)
 
     @classmethod
     def fromitems(cls, items, UF, N, verbose=False, **kwargs):
+        """Create a UFCounter from an iterable of connections."""
         oc = cls(UF, N, **kwargs)
-        oc.M = len(items)             # total number of connections
+        oc.M = len(items)  # total number of connections
         oc.ops = 1 + np.arange(oc.M)  # number of operations
         iters = tqdm(items) if verbose else items
         for p, q in iters:
