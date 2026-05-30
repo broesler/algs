091f34f0c474fdd6a3bbf7860362b5596e30e790
Bernard Roesler <bernard.roesler@gmail.com>
2026-05-29 21:34:05 -0400

    MNT: Update sort_tests style.

--- exercises/sort_tests.py
+++ exercises/sort_tests.py
@@ -1,31 +1,55 @@
 #!/usr/bin/env python3
-#==============================================================================
+# ==============================================================================
 #     File: test_sort.py
 #  Created: 2019-03-15 00:28
 #   Author: Bernie Roesler
-#
-"""
-  Description: Test sorting functions with basic inputs.
-"""
-#==============================================================================
+# ==============================================================================
 
-import string
+"""Test sorting functions with basic inputs."""
+
 import random
+import string
 
-from algs.sort import is_sorted, bubble_sort, insertion_sort, mergesort, \
-                      mergesort_BU, qsort, heap_sort, \
-                      quicksort0, quicksort0r, qsort0, qsort1, qsort2
+from algs.sort import (
+    bubble_sort,
+    heap_sort,
+    insertion_sort,
+    is_sorted,
+    mergesort,
+    mergesort_BU,
+    qsort,
+    qsort0,
+    qsort1,
+    qsort2,
+    quicksort0,
+    quicksort0r,
+)
 
-def should_be(a, b):
-    """Test a condition."""
-    global tests, fails
-    tests += 1
-    try:
-        assert a == b
-    except AssertionError as e:
-        fails += 1
-        print(f"[{test_name}]: Got: {a}, Expected: {b}")
-        raise e
+
+class TestRunner:
+    """Class to run tests and keep track of results."""
+
+    def __init__(self):
+        self.tests = 0
+        self.fails = 0
+
+    def should_be(self, a, b):
+        """Test a condition."""
+        self.tests += 1
+        try:
+            assert a == b
+        except AssertionError as e:
+            self.fails += 1
+            print(f"[{test_name}]: Got: {a}, Expected: {b}")
+            raise e
+
+    def print_summary(self):
+        """Print a summary of the test results."""
+        if self.fails > 0:
+            print(f"{self.fails}/{self.tests} tests failed")
+        else:
+            print(f"All {self.tests} tests passed!")
+
 
 # Define test cases
 ints = [8, 4, 3, 2, 1, 7, 6, 0, 5, 9]
@@ -33,39 +57,43 @@ ints = [8, 4, 3, 2, 1, 7, 6, 0, 5, 9]
 chrs = list('SORTEXAMPLE')
 
 s = string.ascii_uppercase
-strs = [s[i:i+3] for i in range(0, len(s), 3)]
+strs = [s[i : i + 3] for i in range(0, len(s), 3)]
 random.shuffle(strs)
 
-test_As = [(ints, sorted(ints)),
-           (chrs, sorted(chrs)),
-           (strs, sorted(strs))]
+test_As = [(ints, sorted(ints)), (chrs, sorted(chrs)), (strs, sorted(strs))]
 
-sort_funs = [bubble_sort, insertion_sort, mergesort, mergesort_BU,
-             quicksort0, quicksort0r, qsort0, qsort1, qsort2, qsort,
-             heap_sort]
+sort_funs = [
+    bubble_sort,
+    insertion_sort,
+    mergesort,
+    mergesort_BU,
+    quicksort0,
+    quicksort0r,
+    qsort0,
+    qsort1,
+    qsort2,
+    qsort,
+    heap_sort,
+]
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 #        Run general sorting algorithm tests
-#------------------------------------------------------------------------------
-tests = 0
-fails = 0
+# ------------------------------------------------------------------------------
+runner = TestRunner()
 
 for A, S in test_As:
-    should_be(is_sorted(S), True)
+    runner.should_be(is_sorted(S), True)
     for sort in sort_funs:
         test_name = sort.__name__
-        should_be(sort([]), [])                # empty list
-        should_be(sort([0]), [0])              # single element list
-        should_be(sort([1, 1, 1]), [1, 1, 1])  # all equal
-        should_be(sort(A) is not A, True)      # return a copy
-        should_be(sort(list(A)), S)            # randomized A
-        should_be(sort(list(S)), S)            # sorted list
-        should_be(sort(list(S[::-1])), S)      # reverse sorted list
+        runner.should_be(sort([]), [])  # empty list
+        runner.should_be(sort([0]), [0])  # single element list
+        runner.should_be(sort([1, 1, 1]), [1, 1, 1])  # all equal
+        runner.should_be(sort(A) is not A, True)  # return a copy
+        runner.should_be(sort(list(A)), S)  # randomized A
+        runner.should_be(sort(list(S)), S)  # sorted list
+        runner.should_be(sort(list(S[::-1])), S)  # reverse sorted list
 
-if fails > 0:
-    print(f"{fails}/{tests} tests failed")
-else:
-    print(f"All {tests} tests passed!")
+runner.print_summary()
 
-#==============================================================================
-#==============================================================================
+# ==============================================================================
+# ==============================================================================
