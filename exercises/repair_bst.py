f11cec91d03f11c1bea4cb1cf155bb18066dfc4a
Bernard Roesler <bernard.roesler@gmail.com>
2026-05-29 21:11:08 -0400

    MNT: Update repair_bst style. FIXME needed.

--- exercises/repair_bst.py
+++ exercises/repair_bst.py
@@ -3,17 +3,19 @@
 #     File: repair_bst.py
 #  Created: 2021-03-01 17:42
 #   Author: Bernie Roesler
-#
-"""
-  Description: Find two swapped keys in a BST and repair the error.
-    See: <https://algs4.cs.princeton.edu/32bst/> for more info.
-"""
 # =============================================================================
+
+"""
+3.2, Web Exercise 8: Find two swapped keys in a BST and repair the error.
+See: <https://algs4.cs.princeton.edu/32bst/> for more info.
+"""
 
 from algs.search import BST
 
+
 class SelfHealingBST(BST):
     """A BST with methods to find and repair swapped nodes."""
+
     def _find_swapped_keys(self):
         """Find the indices of a pair of keys that are out of order.
 
@@ -23,13 +25,16 @@ class SelfHealingBST(BST):
             Ranks of the keys that are out of order.
         """
         p = q = None
-        keys = list(self._iterate_keys())  # does not rely on self.min()
-        for i in range(self.size-1):
+        # FIXME original line works, but updated self.keys() does not?
+        # See commit: 8836d2^, where change was made.
+        # keys = list(self._iterate_keys())  # does not rely on self.min()
+        keys = self.keys()  # does not rely on self.min()
+        for i in range(self.size - 1):
             if keys[i] > keys[i+1]:
                 if p is None:
                     p = i
                 elif q is None:
-                    q = i+1
+                    q = i + 1
                 else:
                     break
         if p is None:
@@ -40,7 +45,7 @@ class SelfHealingBST(BST):
 
     def _swap_keys(self, p, q):
         """Swap the nodes corresponding to keys of rank `p` and `q`.
-            
+
         Parameters
         ----------
         p, q : int
@@ -66,17 +71,17 @@ class SelfHealingBST(BST):
 st = SelfHealingBST.fromkeys(list('SEARCHEXAMPLE'))
 assert st._is_ordered()
 st._swap_keys(st.rank('E'), st.rank('M'))  # must use rank
-assert st._is_ordered() == False
+assert not st._is_ordered()
 st.repair()
-assert st._is_ordered()
+assert st._is_ordered()  # FIXME
 
 # Test multiple swaps
 st = SelfHealingBST.fromkeys(list('SEARCHEXAMPLE'))
 st._swap_keys(st.rank('E'), st.rank('M'))  # must use rank
-st._swap_keys(2, st.size-1)  # must use rank indices, since swaps break BST
-assert st._is_ordered() == False
+st._swap_keys(2, st.size() - 1)  # must use rank indices, since swaps break BST
+assert not st._is_ordered()
 st.repair()
-assert st._is_ordered()
+assert st._is_ordered()  # FIXME
 
 # =============================================================================
 # =============================================================================
