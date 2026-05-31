1e05d020f748c436d6a1309b022325e40cbb18a1
Bernard Roesler <bernard.roesler@gmail.com>
2026-05-30 20:07:41 -0400

    ENH: Update implementation of count_lines to handle empty files.

--- exercises/util.py
+++ exercises/util.py
@@ -3,19 +3,17 @@
 #     File: util.py
 #  Created: 2022-06-07 11:16
 #   Author: Bernie Roesler
-#
-"""
-Utility functions used in exercises.
-"""
 # =============================================================================
+
+"""Utility functions used in exercises."""
 
 
 def count_lines(fp):
     """Scan through file to count the number of lines."""
-    for i, line in enumerate(fp, 1):
-        pass
+    num_lines = sum(1 for _ in fp)
     fp.seek(0)  # rewind file
-    return i
+    return num_lines
+
 
 # =============================================================================
 # =============================================================================
