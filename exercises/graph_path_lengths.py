bb97dc374475412faa57271262c4bf56de51d0ee
Bernard Roesler <bernard.roesler@gmail.com>
2026-05-30 21:16:49 -0400

    MNT: Refactor graph_path_lengths into modern pandas usage.

--- exercises/graph_path_lengths.py
+++ exercises/graph_path_lengths.py
@@ -11,39 +11,37 @@ Test probability of finding a path between two random vertices and length of
 path when found.
 
 Inputs:
-* N graphs generated
-* V
-* E -> E/V gives sparsity. E in [V-1, V(V-1)/2] for connected graph, so
-       1 < E/V < V^0.5 is sparse, E/V ~ V is dense
+    * N graphs generated
+    * V
+    * E -> E/V gives sparsity. E in [V-1, V(V-1)/2] for connected graph, so
+      1 < E/V < V^0.5 is sparse, E/V ~ V is dense
 Each graph:
-* T random pairs of vertices attempted in a given graph
+    * T random pairs of vertices attempted in a given graph
 
 Outputs:
 Each trial:
-* whether or not s and t are connected
-* path length if connected
+    * whether or not s and t are connected
+    * path length if connected
 Each graph:
-* fraction of trials that resulted in a path
-* average path length
+    * fraction of trials that resulted in a path
+    * average path length
 Aggregate over N graphs:
-* fraction of trials that resulted in a path
-* average path length
+    * fraction of trials that resulted in a path
+    * average path length
 """
 
-import pickle
 from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 from tqdm import tqdm
 
 from algs.graph import Bipartite, BreadthFirstPaths, CC_nr, DepthFirstPaths_nr
 from algs.graph.random import random_simple_graph
 
-rng = np.random.default_rng(seed=565656)
-
-FORCE_UPDATE = True
+FORCE_UPDATE = False
 SAVE_FIGS = False
 
 generate_graph = random_simple_graph
@@ -52,102 +50,162 @@ tag = 'simple'
 # generate_graph = erdos_renyi
 # tag = 'erdos'
 
-V = 100
+V = 100  # number of vertices in each graph
 
 PKL_PATH = Path(__file__).parent / 'pkl'
-pkl_file = PKL_PATH / f"graph_path_lengths_{tag}_V{V}.pkl"
+pkl_file = PKL_PATH / f"graph_path_lengths_{tag}_V{V}.parquet"
+
+
+def simulate_graphs(V, Es, N, T):
+    """Yield a list of dicts of trial results, for use in a DataFrame.
+
+    Parameters
+    ----------
+    V : int
+        Number of vertices in each graph.
+    Es : array-like
+        List of edge counts to generate graphs with.
+    N : int
+        Number of graphs to generate for each edge count.
+    T : int
+        Number of trials to run for each graph.
+
+    Yields
+    ------
+    result : dict
+        Dictionary containing results for each trial, with keys:
+        - 'E': Number of edges in the graph.
+        - 'components': Number of connected components in the graph.
+        - 'bipartite_depth': Count related to bipartite property of the graph.
+        - 'depth': Length of path found by DFS (or NaN if no path).
+        - 'breadth': Length of path found by BFS (or NaN if no path).
+    """
+    rng = np.random.default_rng(seed=565656)
+
+    for E in tqdm(Es):
+        for _ in range(N):
+            G = generate_graph(V, E)
+            cc_count = CC_nr(G).count()
+            bp_count = Bipartite(G)._count
+
+            for _ in range(T):
+                s, t = rng.integers(V, size=2)
+                dfs = DepthFirstPaths_nr(G, s)
+                bfs = BreadthFirstPaths(G, s)
+
+                yield {
+                    'E': E,
+                    'components': cc_count,
+                    'bipartite_depth': bp_count,
+                    'depth': len(dfs.path_to(t)) if dfs.has_path_to(t) else np.nan,
+                    'breadth': len(bfs.path_to(t)) if bfs.has_path_to(t) else np.nan,
+                }
+
 
 if FORCE_UPDATE or not pkl_file.exists():
     N = 30  # graphs to generate
     T = 10  # trials per graph
     Es = np.geomspace(V**0.5, V * (V - 1) // 2, num=20).astype(int)
 
-    index = pd.MultiIndex.from_product((Es, range(N), range(T)), names=['E', 'N', 'T'])
-    data = np.full((len(Es) * N * T, 3), np.nan)
-    df = pd.DataFrame(index=index, columns=['depth', 'breadth', 'comps'], data=data)
-    cf = pd.Series(
-        index=index.droplevel(-1).drop_duplicates(), data=np.full(len(Es) * N, np.nan)
-    )
-    bf = pd.Series(
-        index=index.droplevel(-1).drop_duplicates(), data=np.full(len(Es) * N, np.nan)
-    )
-
-    for E in tqdm(Es):
-        for i in range(N):
-            G = generate_graph(V, E)
-            cc = CC_nr(G)
-            bp = Bipartite(G)
-            cf.loc[(E, i)] = cc.count()
-            bf.loc[(E, i)] = bp._count
-            for j in range(T):
-                s, t = rng.integers(V, size=2)
-                dfs = DepthFirstPaths_nr(G, s)
-                bfs = BreadthFirstPaths(G, s)
-                if dfs.has_path_to(t):
-                    df.loc[(E, i, j), 'depth'] = len(dfs.path_to(t))
-                if bfs.has_path_to(t):
-                    df.loc[(E, i, j), 'breadth'] = len(bfs.path_to(t))
-
-    # Average components over trials
-    cf = cf.groupby('E').mean()
-    bf = bf.groupby('E').mean()
-
-    # Average path lengths over trials
-    tf = (
-        df.reset_index()
-        .assign(count=lambda x: ~np.isnan(x['depth']) / (N * T))
+    # Build the DataFrame
+    df = (
+        pd.DataFrame(simulate_graphs(V, Es, N, T))
         .groupby('E')
-        .agg({'depth': np.nanmean, 'breadth': np.nanmean, 'count': np.sum})
-        .assign(VoE=V / Es)
-        .assign(EoV=Es / V)
-        .assign(components=cf)
-        .assign(bipartite_depth=bf)
+        # Average path lengths over trials
+        .agg(
+            depth=('depth', 'mean'),
+            breadth=('breadth', 'mean'),
+            components=('components', 'mean'),
+            bipartite_depth=('bipartite_depth', 'mean'),
+            valid_paths=('depth', 'count'),
+        )
+        .assign(
+            count=lambda df_: df_['valid_paths'] / (N * T),
+            EoV=lambda df_: df_.index / V,  # E is index
+        )
+        .drop(columns='valid_paths')
     )
 
-    with pkl_file.open('wb') as fp:
-        pickle.dump((tf, cf, bf), fp)
+    df.to_parquet(pkl_file)
 else:
-    with pkl_file.open('rb') as fp:
-        tf, cf, bf = pickle.load(fp)
+    df = pd.read_parquet(pkl_file)
 
 
 # -----------------------------------------------------------------------------
 #         Plot
 # -----------------------------------------------------------------------------
-print(tf)
+print(df)
 
-fig = plt.figure(1, clear=True, constrained_layout=True)
-ax = fig.add_subplot()
-ax.scatter(tf['EoV'], tf['depth'], c='C0', zorder=3, label='DFS')
-ax.scatter(tf['EoV'], tf['breadth'], c='C3', zorder=3, label='BFS')
-ax.scatter(tf['EoV'], cf.values, c='C2', zorder=3, label='CC')
-ax.scatter(tf['EoV'], bf.values, c='C4', zorder=3, label='BP')
+rename_map = {
+    'depth': 'DFS',
+    'breadth': 'BFS',
+    'components': 'CC',
+    'bipartite_depth': 'BP',
+}
+
+color_map = {
+    'DFS':'tab:blue',
+    'BFS':'tab:red',
+    'CC': 'tab:green',
+    'BP': 'tab:purple',
+}
+
+df_tidy = (
+    df.rename(columns=rename_map)
+    .melt(
+        id_vars=['EoV'],
+        value_vars=list(rename_map.values()),
+        var_name='metric',
+        value_name='value',
+    )
+)
+
+fig, ax = plt.subplots(1, clear=True)
+ax_twin = ax.twinx()
+
+sns.scatterplot(
+    data=df_tidy,
+    x='EoV',
+    y='value',
+    hue='metric',
+    palette=color_map,
+    ax=ax,
+)
+
+ax_twin.plot(df['EoV'], df['count'], color='k', alpha=0.5)
+
 # ax.axvline(1/2 * np.log(V), c='k', ls='--', alpha=0.5)
 ax.axhline(V / 2, c='k', ls='--', alpha=0.5)
 
-ax.set(xlabel='density (E/V)', xscale='log', ylabel='path length, # components')
+ax.set(
+    xlabel='density (E/V)',
+    xscale='log',
+    ylabel='path length, # components',
+    ylim=(0, 1.03 * V),
+)
+ax_twin.set(
+    ylabel='fraction connected',
+    ylim=(0, 1.03),
+)
+
 ax.set_xticks([V**-0.5, 1, V**0.5, (V - 1) / 2])
 ax.set_xticklabels([r'$\frac{\sqrt{V}}{V}$', '1', r'$\sqrt{V}$', r'$\frac{V-1}{2}$'])
-ax.set_yticks(V * np.arange(0, 1.2, 0.2))
-ax.set_yticklabels(['0'] + [f"{x:.1f}V" for x in np.arange(0.2, 1, 0.2)] + ['V'])
+
+y_ticks = np.arange(0, 1.2, 0.2)
+ax.set_yticks(V * y_ticks)
+ax.set_yticklabels(['0'] + [f"{x:.1f}V" for x in y_ticks[1:-1]] + ['V'])
+
 # ax.set_yticks([0, V//4, V//2, 3*V//4, V])
 # ax.set_yticklabels(
 #     ['0', r'$\frac{V}{4}$', r'$\frac{V}{2}$', r'$\frac{3V}{4}$', r'$V$']
 # )
-ax.legend()
-ax.set_ylim(top=1.03 * V)
-ax.grid(which='both')
 
-# Plot the count fraction on the other axis
-ax1 = ax.twinx()
-ax1.plot(tf['EoV'], tf['count'], c='k', alpha=0.5)
-ax1.set_ylabel('fraction connected')
-# Match tick marks
-ax1.set_ylim(top=1.03)
-ax1.grid(visible=False)
+ax.grid(which='both')
+ax_twin.grid(visible=False)
 
 if SAVE_FIGS:
-    fig.savefig(f"./figures/graph_path_lengths_{tag}_V{V}.png")
+    FIG_PATH = Path(__file__).parent / 'figures'
+    fig.savefig(FIG_PATH / f"graph_path_lengths_{tag}_V{V}.pdf")
 
 plt.show()
 
