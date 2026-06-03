7044b3f8fb6daaef5dbe55f735d2371d491bf0d0
Bernard Roesler <bernard.roesler@gmail.com>
2026-06-02 23:31:47 -0400

    MNT: Update frequency_count_plots to match new data schema.

--- exercises/frequency_count_plots.py
+++ exercises/frequency_count_plots.py
@@ -7,160 +7,151 @@
 
 """Plot amortized cost of various types of searches."""
 
-import pickle
 from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
-
-# TODO
-# * update label coords to just use `labelpad=-15`
-# * update `np.array` calls to `np.r_`, etc.
-
-SAVE_FIGS = False
-
-MINLEN = 8  # 1, 8, 10
+import pandas as pd
 
 DATA_PATH = Path(__file__).parent.parent / 'data'
 PKL_PATH = Path(__file__).parent / 'pkl'
 
-filename = DATA_PATH / 'tiny_tale.txt'  # 292
-# filename = DATA_PATH / 'tale.txt'  # 779K
-# filename = DATA_PATH / 'leipzig1m.txt'  # 124M
+SAVE_FIGS = False
 
-# ST_names = ['ArrayST', 'BinarySearchST', 'BST', 'RedBlackBST']
-# ST_names = ['ArrayST', 'RedBlackBST', 'SeparateChainingHashST']
-ST_names = ['SeparateChainingHashST', 'LinearProbingHashST']
-
-tag = filename.stem
+MINLEN = 8  # 1, 8, 10
 kind = 'resize'  # 'ins', 'app', 'selforg', 'cache', 'resize'
 
-fig = plt.figure(0, clear=True)
-fig.set_size_inches((6, max(8, 3 * len(ST_names))), forward=True)
-fig.suptitle(f"{filename.name}, min. length = {MINLEN}")
-gs = fig.add_gridspec(nrows=len(ST_names), ncols=1)
+# filestem = 'tiny_tale'  # 292
+filestem = 'tale'       # 779K
+# filestem = 'leipzig1m'  # 124M
 
-for i, ST_name in enumerate(ST_names):
-    # kind = 'resize' if ST_name == 'SeparateChainingHashST' else 'app'
+ST_names = ['ArrayST', 'BinarySearchST', 'BST', 'RedBlackBST']
+# ST_names = ['ArrayST', 'RedBlackBST', 'SeparateChainingHashST']
+# ST_names = ['SeparateChainingHashST', 'LinearProbingHashST']
 
-    # Load the FrequencyCounter
-    pkl_file = PKL_PATH / f"{tag}_{ST_name}_m{MINLEN:02d}_{kind}.pkl"
-    with pkl_file.open('rb') as fp:
-        fc = pickle.load(fp)
+# Load the summary data
+df = pd.read_parquet(PKL_PATH / f"frequency_count_summary_{kind}.parquet")
+df = df.rename({'file': 'filestem'}, axis=1)
 
-    ops = np.arange(fc.N)  # one operation per word in input
-    mean_cmp = np.cumsum(fc.cost)[1:] / ops[1:]  # cumulative average cost
+# Filter for these particular (minlen, filestem, ST_names) combinations
+df = df.loc[
+    (df['minlen'] == MINLEN) & (df['filestem'] == filestem) & (df['alg'].isin(ST_names))
+]
+
+# -----------------------------------------------------------------------------
+#         Plot amortized cost of each search type
+# -----------------------------------------------------------------------------
+fig, axs = plt.subplots(num=0, nrows=len(ST_names), sharex=True, clear=True)
+fig.set_size_inches((6.4, min(8, max(4.8, 3 * len(ST_names)))), forward=True)
+fig.suptitle(f"{filestem}.txt, min. length = {MINLEN}")
+
+for ax, ST_name in zip(axs.flat, ST_names):
+    # Get the individual row for this (minlen, filestem, ST_name)
+    tf = df.loc[df['alg'] == ST_name]
+
+    if len(tf) != 1:
+        raise ValueError(f"Expected exactly one row for {ST_name}, got {len(tf)}")
+
+    tf = tf.iloc[0]
+
+    # Load the data
+    data = np.load(tf['trace_file'])
+    cost, time = data['cost'], data['time']
+
+    ops = np.arange(tf['words'])  # one operation per word in input
+    mean_cmp = np.cumsum(cost)[1:] / ops[1:]  # cumulative average cost
 
     # Plot the amortized cost (# cost) vs. number of `put` operations
-    ax = fig.add_subplot(gs[i])
-    # ax.set_title(ST_name, fontsize=12)
     note = ST_name
+
     if ST_name == 'SeparateChainingHashST':
-        note += f" (M = {fc.t.M})"
+        note += f" (M = {tf['M']})"
+
     ax.annotate(
         note, xy=(0.01, 0.97), xycoords='axes fraction', ha='left', va='top', color='k'
     )
 
-    # Place labels on axis (like ticklabels)
-    ax.set_xlabel('operations')
-    ax.xaxis.set_label_coords(
-        np.mean(ops), 0, transform=ax.xaxis.get_ticklabels()[0].get_transform()
-    )
-
-    ax.set_ylabel('cost')
-    ax.yaxis.set_label_coords(
-        0, max(fc.cost) / 2, transform=ax.yaxis.get_ticklabels()[0].get_transform()
-    )
-
-    ax.scatter(ops, fc.cost, c=0.7 * np.array([[1, 1, 1]]), s=1, alpha=0.8)
-    ax.plot(ops[1:], mean_cmp, 'C3-')
+    # Plot
+    ax.scatter(ops, cost, c='0.7', s=1, alpha=0.8)
+    ax.plot(ops[1:], mean_cmp, c='tab:red')
 
     ax.annotate(
         rf"$\leftarrow$ {mean_cmp[-1]:.1f}",
-        xy=(fc.N, mean_cmp[-1]),
+        xy=(tf['words'], mean_cmp[-1]),
         ha='left',
         va='center',
-        color='C3',
+        color='tab:red',
     )
 
-    ax.xaxis.label.set_color('C3')
-    ax.yaxis.label.set_color('C3')
-    ax.set_xlim([0, fc.N])
-    ax.set_ylim([0, max(fc.cost)])
-    ax.set_xticks([0, fc.N])
-    ax.set_yticks([0, max(fc.cost)])
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    # Hide x-labels except for bottom
-    if i < len(ST_names) + 1:
-        ax.xaxis.set_ticklabels([])
-        ax.set_xlabel('')
-    else:
-        ax.ticklabel_format(style='plain')  # no scientific notation
+    ax.set_ylabel('cost', c='tab:red', labelpad=-15)
+    ax.set(
+        xlim=[0, tf['words']],
+        ylim=[0, max(cost)],
+        xticks=[0, tf['words']],
+        xticklabels=[0, f"{tf['words']:}"],
+        yticks=[0, max(cost)],
+    )
 
-# gs.tight_layout(fig, rect=(0, 0, 1, 0.96))
-gs.tight_layout(fig)
+    # Place labels on axis (like ticklabels)
+    if ax.get_subplotspec().is_last_row():
+        ax.set_xlabel('operations', c='tab:red', labelpad=-12)
+
+    ax.spines[['top', 'right']].set_visible(False)
+
 
 if SAVE_FIGS:
     FIG_PATH = Path(__file__).parent / 'figures'
-    figname = FIG_PATH / f"{tag}_M{MINLEN:02d}_{kind}_frequency_count.pdf"
+    fig_stem = f"{filestem}_M{MINLEN:02d}_{kind}"
+    figname = FIG_PATH / f"{fig_stem}_frequency_count.pdf"
     fig.savefig(figname)
 
 
 # -----------------------------------------------------------------------------
 #         Plot actual timings
 # -----------------------------------------------------------------------------
-# ST_names = ['ArrayST', 'BinarySearchST', 'BST', 'ArrayBST', 'RedBlackBST',
-#             'SeparateChainingHashST']
-ST_names = [
-    'ArrayST',
-    'BinarySearchST',
-    'BST',
-    'ArrayBST',
-    'RedBlackBST',
-    'SeparateChainingHashST',
-    'LinearProbingHashST',
-]
+# ST_names = [
+#     'ArrayST',
+#     'BinarySearchST',
+#     'BST',
+#     'ArrayBST',
+#     'RedBlackBST',
+#     'SeparateChainingHashST',
+#     'LinearProbingHashST',
+# ]
 
-# tag = filename.stem
-# kind = 'app'  # 'ins', 'app', 'selforg', 'cache'
-
-fig = plt.figure(1, clear=True)
-fig.suptitle(f"{filename.name}, min. length = {MINLEN}")
-ax = fig.add_subplot()
+fig, ax = plt.subplots(num=1, clear=True)
+fig.suptitle(f"{filestem}.txt, min. length = {MINLEN}")
 
 for ST_name in ST_names:
     kind = 'resize' if 'hash' in ST_name.lower() else 'app'
 
-    # Load the FrequencyCounter
-    pkl_file = PKL_PATH / f"{tag}_{ST_name}_m{MINLEN:02d}_{kind}.pkl"
-    with pkl_file.open('rb') as fp:
-        fc = pickle.load(fp)
+    # Get the individual row for this (minlen, filestem, ST_name)
+    tf = df.loc[df['alg'] == ST_name]
 
-    ops = np.arange(fc.N)  # one operation per word in input
-    mean_cmp = np.cumsum(fc.time)[1:]  # cumulative average cost
+    if len(tf) != 1:
+        raise ValueError(f"Expected exactly one row for {ST_name}, got {len(tf)}")
+
+    tf = tf.iloc[0]
+
+    # Load the data
+    data = np.load(tf['trace_file'])
+    cost, time = data['cost'], data['time']
+
+    ops = np.arange(tf['words'])  # one operation per word in input
+    mean_cmp = np.cumsum(time)[1:]  # cumulative average cost
 
     # Plot the cumulative runtime vs. number of `put` operations
     ax.plot(ops[1:], mean_cmp, label=ST_name)
 
 # Format the axes
 ax.legend()
-
-# Place labels on axis (like ticklabels)
 ax.set(xscale='log', yscale='log')
-# ax.set(xscale='log')
-ylim = ax.get_ylim()
-ax.set_xlabel('operations')
-ax.set_ylabel('time [s]')
-ax.xaxis.label.set_color('C3')
-ax.yaxis.label.set_color('C3')
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False)
-
-fig.tight_layout()
+ax.set_xlabel('operations', c='tab:red')
+ax.set_ylabel('time [s]', c='tab:red')
+ax.spines[['top', 'right']].set_visible(False)
 
 if SAVE_FIGS:
-    figname = Path(f"./figures/{tag}_M{MINLEN:02d}_{kind}_runtime.pdf")
+    figname = FIG_PATH / f"{fig_stem}_runtime.pdf"
     fig.savefig(figname)
 
 plt.show()
